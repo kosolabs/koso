c5ccb8918b951f283739dff031071949df0342f8
Shad Sharma <shadanan@gmail.com>
2024-07-11 21:10:59 -0700

    productionize (#43)
    - **Add systemd service definition and deploy script**
    - **Make executable**
    - **Print commands as they are executed**
    - **Add DB URL and if check**

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -x
+
+git fetch
+if git merge-base --is-ancestor origin/main main; then
+  exit 0
+fi
+git pull
+
+pushd frontend
+npm run build
+popd
+
+pushd backend
+cargo build --release
+DATABASE_URL=postgresql://koso:koso@localhost/koso sqlx migrate run
+popd
+
+systemctl restart koso
