06e5a2bdc2753ee748cfb4b7865ed7537b50d444
Kyle Leonhard <leonhard.kyle@gmail.com>
2024-07-16 16:19:30 -0700

    Notify via Telegram on deploy failure

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env zsh
 
+set -e
+
+function _on_fail {
+    telegram "Failed to deploy $(git rev-parse --short HEAD) \\- $(git log --format=%s%b -n 1 HEAD | telegram_escape)" "❌"
+}
+trap _on_fail ZERR
+
 source /root/.telegram.zsh
 
 pushd frontend
@@ -14,4 +21,4 @@ popd
 systemctl daemon-reload
 systemctl restart koso
 
-telegram "Deployed $(git rev-parse --short HEAD) \\- $(git log --format=%s%b -n 1 HEAD | telegram_escape)"
+telegram "Deployed $(git rev-parse --short HEAD) \\- $(git log --format=%s%b -n 1 HEAD | telegram_escape)" "✅"
