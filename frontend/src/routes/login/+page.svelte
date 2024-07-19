7f18fb46cb1f0057a22d172634c41190bd5c2628
Shad Sharma <shadanan@gmail.com>
2024-07-18 18:18:13 -0700

    Show logged in status

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -1,8 +1,25 @@
 <script lang="ts">
+  import type { User } from "$lib/auth";
+  import { Avatar } from "flowbite-svelte";
   import { GoogleOAuthProvider } from "google-oauth-gsi";
+  import { jwtDecode } from "jwt-decode";
   import { onMount } from "svelte";
 
-  let credential: string | null = null;
+  const CREDENTIAL_KEY = "credential";
+
+  let token: string | null = sessionStorage.getItem(CREDENTIAL_KEY) || null;
+
+  function decodeUser(token: string | null): User | null {
+    if (token === null) {
+      return null;
+    }
+    const user = jwtDecode(token) as User;
+    if (user.exp * 1000 < Date.now()) {
+      return null;
+    }
+    return user;
+  }
+  $: user = decodeUser(token);
 
   onMount(() => {
     const googleProvider = new GoogleOAuthProvider({
@@ -19,8 +36,8 @@
               console.error("Credential is missing", res);
               return;
             }
-            credential = res.credential;
-            console.log(res);
+            token = res.credential;
+            sessionStorage.setItem(CREDENTIAL_KEY, res.credential);
           },
         })();
       },
@@ -29,10 +46,14 @@
 </script>
 
 <div class="m-auto w-96 rounded border bg-slate-100 p-10 text-center">
-  <h1 class="h1 mb-8 text-2xl">Sign in to Koso</h1>
-  {#if credential}
-    {credential}
-  {:else}
-    <div id="google-login-button" />
+  <h1 class="mb-8 text-2xl">Sign in to Koso</h1>
+  {#if user}
+    <div
+      class="flex items-center justify-center rounded-full border bg-slate-200 p-2"
+    >
+      <div><Avatar src={user.picture} /></div>
+      <div class="pl-2">{user.name}</div>
+    </div>
   {/if}
+  <div id="google-login-button" hidden={!!token} />
 </div>
