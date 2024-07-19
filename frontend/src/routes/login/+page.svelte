e918809c2ca8198f6b34cce2d8ef75d5aefee830
Shad Sharma <shadanan@gmail.com>
2024-07-18 18:03:40 -0700

    Get a JWT token on login

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -1,33 +1,38 @@
 <script lang="ts">
-  import { GoogleOAuthProvider, googleLogout } from "google-oauth-gsi";
+  import { GoogleOAuthProvider } from "google-oauth-gsi";
   import { onMount } from "svelte";
 
-  export const googleProvider = new GoogleOAuthProvider({
-    clientId:
-      "560654064095-kicdvg13cb48mf6fh765autv6s3nhp23.apps.googleusercontent.com",
-    onScriptLoadError: () => console.log("onScriptLoadError"),
-    onScriptLoadSuccess: () => {},
-  });
+  let credential: string | null = null;
 
   onMount(() => {
-    const oneTap = googleProvider.useGoogleOneTapLogin({
-      cancel_on_tap_outside: true,
-      onSuccess: (res) => console.log("Logged in with google one tap", res),
+    const googleProvider = new GoogleOAuthProvider({
+      clientId:
+        "560654064095-kicdvg13cb48mf6fh765autv6s3nhp23.apps.googleusercontent.com",
+      onScriptLoadSuccess: () => {
+        googleProvider.useRenderButton({
+          element: document.getElementById("google-login-button")!,
+          useOneTap: true,
+          width: 300,
+          use_fedcm_for_prompt: true,
+          onSuccess: (res) => {
+            if (!res.credential) {
+              console.error("Credential is missing", res);
+              return;
+            }
+            credential = res.credential;
+            console.log(res);
+          },
+        })();
+      },
     });
-    oneTap();
   });
-
-  const login = googleProvider.useGoogleLogin({
-    flow: "auth-code",
-    onSuccess: (res) => console.log("Logged in with google", res),
-    onError: (err) => console.error("Failed to login with google", err),
-  });
-  // Call login() on button click
 </script>
 
-<button
-  class="absolute left-1 z-50 rounded p-1 opacity-50 outline hover:opacity-100"
-  on:click={() => login()}
->
-  Login
-</button>
+<div class="m-auto w-96 rounded border bg-slate-100 p-10 text-center">
+  <h1 class="h1 mb-8 text-2xl">Sign in to Koso</h1>
+  {#if credential}
+    {credential}
+  {:else}
+    <div id="google-login-button" />
+  {/if}
+</div>
