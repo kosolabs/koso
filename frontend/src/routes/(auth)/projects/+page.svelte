05b0a18ce57e598c6ab07310ea98266cd81609af
Kyle Leonhard <leonhard.kyle@gmail.com>
2024-07-25 14:13:14 -0700

    hack

--- frontend/src/routes/(auth)/projects/+page.svelte
+++ frontend/src/routes/(auth)/projects/+page.svelte
@@ -4,6 +4,7 @@
   import { Alert, Button, A } from "flowbite-svelte";
   import { goto } from "$app/navigation";
   import { createProject, fetchProjects, type Project } from "$lib/projects";
+  import { Layers } from "lucide-svelte";
 
   let projects: Promise<Project[]> = new Promise(() => {});
   let errorMessage: string | null = null;
@@ -29,34 +30,45 @@
   });
 </script>
 
-<div class="flex flex-col rounded border p-4">
-  {#await projects}
-    <div>Loading...</div>
-  {:then projects}
-    {#if projects.length === 0}
-      <div class="m-auto mb-8 text-xl">Create your first Koso project!</div>
-      <div class="m-auto flex items-end gap-2">
+{#await projects}
+  <div class="flex flex-col items-center justify-center rounded border p-4">
+    <div class="text-xl">Loading...</div>
+  </div>
+{:then projects}
+  {#if projects.length === 0}
+    <div class="flex flex-col items-center justify-center rounded border p-4">
+      <div class="mb-2"><Layers /></div>
+      <div class="mb-4 text-xl">Create your first Koso project!</div>
+      <div>
+        <Button on:click={() => createProjectt()}>New project</Button>
+      </div>
+      {#if errorMessage}
+        <div class="mt-4">
+          <Alert class="border">{errorMessage}</Alert>
+        </div>
+      {/if}
+    </div>
+  {:else}
+    <div class="flex flex-col rounded border">
+      <div class="roundedborder flex flex-col items-end bg-slate-100 p-2">
         <div>
           <Button on:click={() => createProjectt()}>New project</Button>
         </div>
         {#if errorMessage}
-          <Alert class="mt-8" border>{errorMessage}</Alert>
+          <div class="mt-4 flex-grow-0">
+            <Alert class="border">{errorMessage}</Alert>
+          </div>
         {/if}
       </div>
-    {:else}
-      <div>
-        <div>
-          <Button on:click={() => createProjectt()}>New project</Button>
-        </div>
-        {#if errorMessage}
-          <Alert class="mt-8" border>{errorMessage}</Alert>
-        {/if}
-      </div>
-      <div>
+      <div
+        class="flex flex-col items-stretch [&>*:nth-child(even)]:bg-slate-50"
+      >
         {#each projects as project}
-          <div><A href="projects/{project.project_id}">{project.name}</A></div>
+          <div class="rounded border p-2">
+            <A href="projects/{project.project_id}">{project.name}</A>
+          </div>
         {/each}
       </div>
-    {/if}
-  {/await}
-</div>
+    </div>
+  {/if}
+{/await}
