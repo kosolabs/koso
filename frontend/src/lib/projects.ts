798b1e4f6e8aadaeb8aba631d08f9c6b38ec57f4
Kyle Leonhard <leonhard.kyle@gmail.com>
2024-07-25 11:19:21 -0700

    init

---
+++ frontend/src/lib/projects.ts
@@ -0,0 +1,36 @@
+export type Project = {
+  project_id: string;
+  name: string;
+};
+
+export async function fetchProjects(token: string | null): Promise<Project[]> {
+  const response = await fetch("/api/projects", {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  if (!response.ok) {
+    throw new Error(
+      `Failed to fetch projects: ${response.statusText} (${response.status})`,
+    );
+  }
+  return await response.json();
+}
+
+export async function createProject(token: string | null): Promise<Project> {
+  const response = await fetch("/api/projects", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name: "My Project!" }),
+  });
+  if (!response.ok) {
+    throw new Error(
+      `Failed to create project: ${response.statusText} (${response.status})`,
+    );
+  }
+  return await response.json();
+}
--- frontend/src/routes/(auth)/projects/+page.svelte
+++ frontend/src/routes/(auth)/projects/+page.svelte
@@ -1,49 +1,31 @@
 <script lang="ts">
-  import { token } from "$lib/auth";
+  import { token, user } from "$lib/auth";
   import { onMount } from "svelte";
   import { Alert, Button, A } from "flowbite-svelte";
   import { goto } from "$app/navigation";
-
-  type Project = {
-    project_id: string;
-    name: string;
-  };
+  import { createProject, fetchProjects, type Project } from "$lib/projects";
 
   let projects: Promise<Project[]> = new Promise(() => {});
   let errorMessage: string | null = null;
 
-  async function fetchProjects() {
-    const response = await fetch("/api/projects", {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${$token}`,
-      },
-    });
-    return await response.json();
-  }
-
-  async function createProject() {
+  async function createProjectt() {
     errorMessage = null;
-
-    const response = await fetch("/api/projects", {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${$token}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ name: "My Project!" }),
-    });
-    if (!response.ok) {
-      console.log("failed create", response);
-      errorMessage = `Failed to create project: ${response.statusText} (${response.status})`;
-    } else {
-      const project: Project = await response.json();
-      goto(`projects/${project.project_id}`);
+    let project;
+    try {
+      project = await createProject($token);
+    } catch (err) {
+      errorMessage = `${err}`;
+      return;
     }
+    await goto(`/projects/${project.project_id}`);
   }
 
   onMount(() => {
-    projects = fetchProjects();
+    if (!$user) {
+      return;
+    }
+
+    projects = fetchProjects($token);
   });
 </script>
 
@@ -55,7 +37,7 @@
       <div class="m-auto mb-8 text-xl">Create your first Koso project!</div>
       <div class="m-auto flex items-end gap-2">
         <div>
-          <Button on:click={() => createProject()}>New project</Button>
+          <Button on:click={() => createProjectt()}>New project</Button>
         </div>
         {#if errorMessage}
           <Alert class="mt-8" border>{errorMessage}</Alert>
@@ -64,7 +46,7 @@
     {:else}
       <div>
         <div>
-          <Button on:click={() => createProject()}>New project</Button>
+          <Button on:click={() => createProjectt()}>New project</Button>
         </div>
         {#if errorMessage}
           <Alert class="mt-8" border>{errorMessage}</Alert>
