b228a72f0a20678b94f54c815d373ee3a4d88382
Shad Sharma <shadanan@gmail.com>
2024-08-04 12:05:31 -0700

    Add User and story and integrate with navbar (#86)

---
+++ frontend/src/lib/user.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { Avatar } from "flowbite-svelte";
+  import type { User } from "./auth";
+  import { cn } from "./utils";
+
+  export let user: User;
+</script>
+
+<div class={cn("flex items-center gap-2")}>
+  <Avatar src={user.picture} rounded />
+  <div class="flex flex-col">
+    <div class="text-sm">{user.name}</div>
+    <div class="text-sm font-medium">{user.email}</div>
+  </div>
+</div>
