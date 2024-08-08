012f020ea368232753cf12a7ff42cd39206ef47c
kyle <leonhard.kyle@gmail.com>
2024-08-07 17:45:49 -0700

    Reconnect interrupted websocket connections (#109)

--- frontend/src/routes/(auth)/projects/[slug]/+page.svelte
+++ frontend/src/routes/(auth)/projects/[slug]/+page.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
+  import { goto } from "$app/navigation";
   import { page } from "$app/stores";
-  import { logout as auth_logout, token, user, type User } from "$lib/auth";
+  import { token, user, type User } from "$lib/auth";
   import { DagTable } from "$lib/DagTable";
   import { Koso } from "$lib/koso";
-  import { disableRedirectOnLogOut, lastVisitedProjectId } from "$lib/nav";
+  import { lastVisitedProjectId } from "$lib/nav";
   import Navbar from "$lib/navbar.svelte";
   import { fetchProjects, type Project, updateProject } from "$lib/projects";
-  import { A, Button, Input } from "flowbite-svelte";
+  import { A, Alert, Button, Input, Modal } from "flowbite-svelte";
   import { UserPlus } from "lucide-svelte";
   import { onDestroy, onMount } from "svelte";
   import * as Y from "yjs";
@@ -16,11 +17,6 @@
 
   let users: User[] = [];
   let project: Project | null = null;
-
-  async function logout() {
-    disableRedirectOnLogOut();
-    auth_logout();
-  }
 
   async function loadUsers() {
     if (!$user || !$token) throw new Error("User is unauthorized");
@@ -104,20 +100,20 @@
   }
 
   let socket: WebSocket | null = null;
+  let showSocketOfflineAlert: boolean = false;
+  let showUnauthorizedModal: boolean = false;
 
-  onMount(async () => {
-    if (!$user || !$token) {
-      return;
-    }
-
-    [users, project] = await Promise.all([loadUsers(), loadProject()]);
+  async function openWebSocket(backoffMs = 1000) {
+    if (!$user || !$token) throw new Error("User is unauthorized");
 
     const host = location.origin.replace(/^http/, "ws");
     const wsUrl = `${host}/api/ws/projects/${projectId}`;
     socket = new WebSocket(wsUrl, ["bearer", $token]);
     socket.binaryType = "arraybuffer";
 
-    socket.onopen = () => {
+    socket.onopen = (event) => {
+      console.log("WebSocket opened", event);
+      showSocketOfflineAlert = false;
       koso.handleClientMessage((update) => {
         if (socket) {
           socket.send(update);
@@ -135,26 +131,65 @@
     };
     socket.onerror = (event) => {
       console.log("WebSocket errored", event);
-      // Error type is not available, so assume unauthorized and logout
-      $lastVisitedProjectId = null;
-      logout();
+      // Errors also trigger onclose events so handle everything there.
     };
     socket.onclose = (event) => {
+      if (socket === null && event.code === 1000) {
+        console.log(
+          `WebSocket closed in onDestroy. Code: ${event.code}, Reason: '${event.reason}' Will not try to reconnect`,
+          event,
+        );
+        return;
+      }
+
+      const UNAUTHORIZED = 3000;
+      if (event.code === UNAUTHORIZED) {
+        console.log(
+          `Unauthorized, WebSocket closed. Code: ${event.code}, Reason: '${event.reason}'. `,
+          event,
+        );
+        // Don't redirect the user back to a project they don't have access too.
+        $lastVisitedProjectId = null;
+        socket = null;
+        showUnauthorizedModal = true;
+        return;
+      }
+
       console.log(
-        `WebSocket closed. Code: ${event.code}, Reason: '${event.reason}''`,
+        `WebSocket closed. Code: ${event.code}, Reason: '${event.reason}'. Will try to reconnect in ${backoffMs} ms.`,
         event,
       );
-      socket = null;
+      showSocketOfflineAlert = true;
+      setTimeout(async () => {
+        if (socket !== null) {
+          await openWebSocket(Math.min(backoffMs * 2, 60000));
+        }
+      }, backoffMs);
     };
 
     while (socket && socket.readyState == WebSocket.CONNECTING) {
       await new Promise((r) => setTimeout(r, 100));
     }
+  }
+
+  onMount(async () => {
+    if (!$user || !$token) {
+      return;
+    }
+
+    [users, project] = await Promise.all([
+      loadUsers(),
+      loadProject(),
+      openWebSocket(),
+    ]);
   });
 
   onDestroy(() => {
     if (socket) {
-      socket.close(1000, "Closed in onDestroy.");
+      if (socket) {
+        socket.close(1000, "Closed in onDestroy.");
+      }
+      socket = null;
     }
   });
 </script>
@@ -187,5 +222,20 @@
     <Button size="xs" title="Share Project"><UserPlus /></Button>
   </svelte:fragment>
 </Navbar>
+
+{#if showSocketOfflineAlert}
+  <div class="mt-4">
+    <Alert class="border">Connection to server lost. Working offline.</Alert>
+  </div>
+{/if}
+
+<Modal title="Unauthorized" bind:open={showUnauthorizedModal}>
+  <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+    You do not have access to the project or the project does not exist.
+  </p>
+  <svelte:fragment slot="footer">
+    <Button on:click={() => goto("/projects")}>Take me home</Button>
+  </svelte:fragment>
+</Modal>
 
 <DagTable {koso} {users} />
