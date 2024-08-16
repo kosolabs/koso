0f010f3ff107bcdf09e988b855390417fd405c1c
Kyle Leonhard <leonhard.kyle@gmail.com>
2024-08-16 11:33:22 -0700

    fix

--- backend/migrations/20240806213215_edit-projects-id-to-project-id.sql
+++ backend/migrations/20240806213215_edit-projects-id-to-project-id.sql
@@ -1,3 +1,3 @@
 alter table projects drop constraint projects_pkey;
 alter table projects rename column id TO project_id;
-alter table tasks add constraint projects_pkey primary key (project_id);
+alter table projects add constraint projects_pkey primary key (project_id);
