0cdd1388a419eaf43e2b184484f9e616d33d71a5
Shad Sharma <shadanan@gmail.com>
2024-08-19 12:07:37 -0700

    Init shadcn and migrate one button

--- frontend/src/lib/DagTable/table.svelte
+++ frontend/src/lib/DagTable/table.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
   import { user, type User } from "$lib/auth";
+  import { Button } from "$lib/components/ui/button";
   import type { Koso } from "$lib/koso";
-  import { Button } from "flowbite-svelte";
-  import { List, ListStart, ListTree, Trash, Unlink } from "lucide-svelte";
-  import { UserRoundPlus, SquarePen } from "lucide-svelte";
+  import {
+    List,
+    ListStart,
+    ListTree,
+    SquarePen,
+    Trash,
+    Unlink,
+    UserRoundPlus,
+  } from "lucide-svelte";
   import { setContext } from "svelte";
   import { flip } from "svelte/animate";
   import Row from "./row.svelte";
@@ -97,23 +104,23 @@
 
 <div class="sticky top-0 z-30 flex gap-2 bg-white py-2">
   {#if $selected}
-    <Button size="xs" on:click={addPeer}>
+    <Button on:click={addPeer}>
       <List class="me-2 w-4" />Add Peer
     </Button>
-    <Button size="xs" on:click={addChild}>
+    <Button on:click={addChild}>
       <ListTree class="me-2 w-4" />Add Child
     </Button>
     {#if $parents[$selected.name].length === 1}
-      <Button size="xs" on:click={remove}>
+      <Button on:click={remove}>
         <Trash class="me-2 w-4" />Delete
       </Button>
     {:else}
-      <Button size="xs" on:click={unlink}>
+      <Button on:click={unlink}>
         <Unlink class="me-2 w-4" />Unlink
       </Button>
     {/if}
   {:else}
-    <Button size="xs" on:click={addRoot}>
+    <Button on:click={addRoot}>
       <ListStart class="me-2 w-4" />Add Root
     </Button>
   {/if}
