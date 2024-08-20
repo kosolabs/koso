7fc7879ff3867554790e81ab78e625793e53aec9
Shad Sharma <shadanan@gmail.com>
2024-08-19 18:54:04 -0700

    Migrate projects picker page to shadcn

--- frontend/src/routes/(auth)/projects/+page.svelte
+++ frontend/src/routes/(auth)/projects/+page.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
   import { goto } from "$app/navigation";
   import { token, user } from "$lib/auth";
+  import { Alert } from "$lib/components/ui/alert";
+  import { Button } from "$lib/components/ui/button";
   import Navbar from "$lib/navbar.svelte";
   import {
     fetchProjects,
     createProject as projectsCreateProject,
     type Project,
   } from "$lib/projects";
-  import { A, Alert, Button } from "flowbite-svelte";
   import { Layers } from "lucide-svelte";
   import { onMount } from "svelte";
 
@@ -37,6 +38,12 @@
 
 <Navbar />
 
+{#if errorMessage}
+  <div class="my-2 flex-grow-0">
+    <Alert variant="destructive">{errorMessage}</Alert>
+  </div>
+{/if}
+
 {#await projects}
   <div class="flex flex-col items-center justify-center rounded border p-4">
     <div class="text-xl">Loading...</div>
@@ -49,30 +56,26 @@
       <div>
         <Button on:click={() => createProject()}>New project</Button>
       </div>
-      {#if errorMessage}
-        <div class="mt-4">
-          <Alert class="border">{errorMessage}</Alert>
-        </div>
-      {/if}
     </div>
   {:else}
     <div class="flex flex-col rounded border">
-      <div class="roundedborder flex flex-col items-end bg-slate-100 p-2">
+      <div class="flex flex-col items-end bg-slate-100 p-2">
         <div>
           <Button on:click={() => createProject()}>New project</Button>
         </div>
-        {#if errorMessage}
-          <div class="mt-4 flex-grow-0">
-            <Alert class="border">{errorMessage}</Alert>
-          </div>
-        {/if}
       </div>
       <div
         class="flex flex-col items-stretch [&>*:nth-child(even)]:bg-slate-50"
       >
         {#each projects as project}
-          <div class="rounded border p-2">
-            <A href="projects/{project.project_id}">{project.name}</A>
+          <div class="border-t p-2">
+            <Button
+              class="text-lg"
+              variant="link"
+              href="projects/{project.project_id}"
+            >
+              {project.name}
+            </Button>
           </div>
         {/each}
       </div>
