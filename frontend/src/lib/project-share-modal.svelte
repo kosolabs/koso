4db03d1482d670428165c0b017890b5a90d7eae1
Kyle Leonhard <leonhard.kyle@gmail.com>
2024-08-20 16:03:38 -0700

    Convert some bits

--- frontend/src/lib/project-share-modal.svelte
+++ frontend/src/lib/project-share-modal.svelte
@@ -15,6 +15,9 @@
     CircleCheck,
   } from "lucide-svelte";
   import { fade } from "svelte/transition";
+  import * as AlertDialog from "$lib/components/ui/alert-dialog";
+  import * as Dialog from "$lib/components/ui/dialog";
+  import { Description } from "./components/ui/alert";
 
   export let open: boolean;
   export let project: Project | null;
@@ -101,96 +104,110 @@
   );
 </script>
 
-<Modal
-  title="Share &quot;{project?.name || ''}&quot;"
+<Modal autoclose outsideclose size="md"></Modal>
+
+<Dialog.Root
   bind:open
-  autoclose
-  outsideclose
-  size="md"
-  on:close={() => {
-    addedOrRemovedMessage = null;
-    filter = "";
+  onOpenChange={(open) => {
+    if (!open) {
+      addedOrRemovedMessage = null;
+      filter = "";
+    }
   }}
 >
-  <div class="flex flex-1 flex-col gap-2">
-    {#if addedOrRemovedMessage}
-      <div transition:fade={{ duration: 350 }}>
-        <Alert color="green" class="flex items-center p-2 font-medium">
-          <span><CircleCheck /></span>
-          <span>{addedOrRemovedMessage}</span>
-        </Alert>
-      </div>
-    {/if}
-    <Input type="text" placeholder="Add people" bind:value={filter}>
-      <UserPlus slot="left" class="h-4 w-4" />
-    </Input>
+  <Dialog.Content>
+    <Dialog.Header>
+      <Dialog.Title>Share &quot;{project?.name || ""}&quot;</Dialog.Title>
+      <Dialog.Description>Manage access to your project.</Dialog.Description>
+    </Dialog.Header>
+    <div class="flex flex-1 flex-col gap-2">
+      {#if addedOrRemovedMessage}
+        <div transition:fade={{ duration: 350 }}>
+          <Alert color="green" class="flex items-center p-2 font-medium">
+            <span><CircleCheck /></span>
+            <span>{addedOrRemovedMessage}</span>
+          </Alert>
+        </div>
+      {/if}
+      <Input type="text" placeholder="Add people" bind:value={filter}>
+        <UserPlus slot="left" class="h-4 w-4" />
+      </Input>
 
-    <Dropdown
-      bind:open={openDropDown}
-      class="max-h-96 overflow-y-auto"
-      style="width: 39.5rem"
-    >
-      <div class="flex flex-col gap-2 p-2">
-        {#if filteredUsers.length > 0}
-          {#each filteredUsers as user}
-            <button
-              title="Add {user.email}"
+      <Dropdown
+        bind:open={openDropDown}
+        class="max-h-96 overflow-y-auto"
+        style="width: 39.5rem"
+      >
+        <div class="flex flex-col gap-2 p-2">
+          {#if filteredUsers.length > 0}
+            {#each filteredUsers as user}
+              <button
+                title="Add {user.email}"
+                on:click={async () => {
+                  await addUser(user);
+                }}
+              >
+                <UserAvatar {user} />
+              </button>
+            {/each}
+          {:else}
+            <button disabled>No people found.</button>
+          {/if}
+        </div>
+      </Dropdown>
+
+      <div class="h3 mt-2">People with access</div>
+      <div
+        class="flex max-h-96 flex-col items-stretch overflow-y-auto [&>*:nth-child(even)]:bg-slate-50"
+      >
+        {#each projectUsers as projectUser}
+          <div class="flex flex-row rounded border p-2">
+            <A
+              size="xs"
+              class="b border-r-2 pr-2"
+              title="Remove {projectUser.email}"
               on:click={async () => {
-                await addUser(user);
+                await removeUser(projectUser, false);
               }}
             >
-              <UserAvatar {user} />
-            </button>
-          {/each}
-        {:else}
-          <button disabled>No people found.</button>
-        {/if}
-      </div>
-    </Dropdown>
-
-    <div class="h3 mt-2">People with access</div>
-    <div
-      class="flex max-h-96 flex-col items-stretch overflow-y-auto [&>*:nth-child(even)]:bg-slate-50"
-    >
-      {#each projectUsers as projectUser}
-        <div class="flex flex-row rounded border p-2">
-          <A
-            size="xs"
-            class="b border-r-2 pr-2"
-            title="Remove {projectUser.email}"
-            on:click={async () => {
-              await removeUser(projectUser, false);
-            }}
-          >
-            <CircleMinus />
-          </A>
-          <div>
-            <UserAvatar user={projectUser} />
+              <CircleMinus />
+            </A>
+            <div>
+              <UserAvatar user={projectUser} />
+            </div>
           </div>
-        </div>
-      {/each}
+        {/each}
+      </div>
     </div>
-  </div>
-</Modal>
+  </Dialog.Content>
+</Dialog.Root>
 
-<Modal bind:open={openWarnSelfRemovalModal} size="xs" autoclose>
-  <div class="text-center">
-    <TriangleAlert
-      class="mx-auto mb-4 h-12 w-12 text-gray-400 dark:text-gray-200"
-    />
-    <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
-      Are you sure? You will <b>immediately lose access</b> if you remove yourself
-      from this project.
-    </h3>
-    <Button
-      color="red"
-      class="me-2"
-      on:click={async () => {
-        if (!$user) throw new Error("User is unauthorized");
-        await removeUser($user, true);
-        await goto("/projects");
-      }}>Yes, I'm sure</Button
-    >
-    <Button color="alternative">No, cancel</Button>
-  </div>
-</Modal>
+<AlertDialog.Root bind:open={openWarnSelfRemovalModal}>
+  <AlertDialog.AlertDialogTrigger asChild>
+    <Button variant="outline">Show Dialog</Button>
+  </AlertDialog.AlertDialogTrigger>
+  <AlertDialog.AlertDialogContent>
+    <AlertDialog.AlertDialogHeader>
+      <AlertDialog.AlertDialogTitle
+        >Are you absolutely sure?</AlertDialog.AlertDialogTitle
+      >
+      <AlertDialog.AlertDialogDescription>
+        <TriangleAlert
+          class="mx-auto mb-4 h-12 w-12 text-gray-400 dark:text-gray-200"
+        />
+        You will <b>immediately lose access</b> if you remove yourself from this
+        project.
+      </AlertDialog.AlertDialogDescription>
+    </AlertDialog.AlertDialogHeader>
+    <AlertDialog.AlertDialogFooter>
+      <AlertDialog.AlertDialogCancel>Cancel</AlertDialog.AlertDialogCancel>
+      <AlertDialog.AlertDialogAction
+        on:click={async () => {
+          if (!$user) throw new Error("User is unauthorized");
+          await removeUser($user, true);
+          await goto("/projects");
+        }}>Yes, I'm sure</AlertDialog.AlertDialogAction
+      >
+    </AlertDialog.AlertDialogFooter>
+  </AlertDialog.AlertDialogContent>
+</AlertDialog.Root>
