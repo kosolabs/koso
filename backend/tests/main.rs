2e3828591f921c97b611e9feb12874efbc792f1b
kyle <leonhard.kyle@gmail.com>
2024-08-26 16:08:02 -0700

    Setup basic database test (#163)

---
+++ backend/tests/main.rs
@@ -0,0 +1,16 @@
+#[cfg(test)]
+mod tests {
+    use sqlx::PgPool;
+
+    #[test_log::test(sqlx::test)]
+    async fn basic_test(pool: PgPool) -> sqlx::Result<()> {
+        let projects: Vec<(String,)> = sqlx::query_as("SELECT project_id FROM projects")
+            .fetch_all(&pool)
+            .await
+            .unwrap();
+        tracing::info!("test");
+        assert_eq!(projects.len(), 1);
+        assert_eq!(projects.first().unwrap().0, "koso-staging");
+        Ok(())
+    }
+}
