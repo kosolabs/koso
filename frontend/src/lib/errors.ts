5a32da7dc7c19aeab59f730faba3378f0db4e65d
Kyle Leonhard <leonhard.kyle@gmail.com>
2024-08-27 11:32:45 -0700

    name

--- frontend/src/lib/errors.ts
+++ frontend/src/lib/errors.ts
@@ -1,7 +1,8 @@
 import { logout } from "./auth";
 
 export function logout_on_authentication_error(response: Response) {
-  if (response.status == 401) {
+  const AUTHENTICATION_ERROR = 401;
+  if (response.status == AUTHENTICATION_ERROR) {
     console.log(
       "Response failed with an unathentication error (401). Logging user out.",
       response,
