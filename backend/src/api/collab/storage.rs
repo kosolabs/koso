ad78c7f70d791fec7a30e68afd9b4c276983d984
kyle <leonhard.kyle@gmail.com>
2024-08-27 11:50:58 -0700

    Add an updates endpoint (#175)

--- backend/src/api/collab/storage.rs
+++ backend/src/api/collab/storage.rs
@@ -21,11 +21,7 @@ pub(super) async fn persist_update(
 }
 
 pub(super) async fn load_doc(project_id: &ProjectId, pool: &PgPool) -> Result<(Doc, usize)> {
-    let updates: Vec<(Vec<u8>,)> =
-        sqlx::query_as("SELECT update_v2 FROM yupdates WHERE project_id=$1")
-            .bind(project_id)
-            .fetch_all(pool)
-            .await?;
+    let updates = load_raw_updates(project_id, pool).await?;
     let update_count = updates.len();
 
     let doc = Doc::new();
@@ -38,4 +34,23 @@ pub(super) async fn load_doc(project_id: &ProjectId, pool: &PgPool) -> Result<(D
         }
     }
     Result::Ok((doc, update_count))
+}
+
+pub async fn load_updates(project_id: &ProjectId, pool: &PgPool) -> Result<Vec<Update>> {
+    let updates = load_raw_updates(project_id, pool).await?;
+    let updates = updates
+        .into_iter()
+        .map(|u| Update::decode_v2(&u.0))
+        .collect::<Result<Vec<_>, yrs::encoding::read::Error>>()?;
+
+    Result::Ok(updates)
+}
+
+async fn load_raw_updates(project_id: &ProjectId, pool: &PgPool) -> Result<Vec<(Vec<u8>,)>> {
+    let updates: Vec<(Vec<u8>,)> =
+        sqlx::query_as("SELECT update_v2 FROM yupdates WHERE project_id=$1")
+            .bind(project_id)
+            .fetch_all(pool)
+            .await?;
+    Ok(updates)
 }
