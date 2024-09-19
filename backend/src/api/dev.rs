ded15323b0e1dce9aa43ba10906cba186ef205c7
kyle <leonhard.kyle@gmail.com>
2024-09-19 14:27:46 -0700

    Cleanup test users and projects automatically (#225)

---
+++ backend/src/api/dev.rs
@@ -0,0 +1,89 @@
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
+
+use crate::api::{internal_error, ApiResult};
+use axum::{routing::post, Extension, Router};
+use sqlx::PgPool;
+
+use super::google;
+
+pub(super) fn dev_router() -> Router {
+    let enable_dev = std::env::var("TESTONLY_ENABLE_DEV").map_or(false, |v| v == "true");
+    if enable_dev {
+        tracing::info!("Enable dev mode. Something is WRONG if you see this in production.");
+        return Router::new().route("/cleanup_test_data", post(cleanup_test_data_handler));
+    }
+
+    Router::new()
+}
+
+#[tracing::instrument(skip(pool))]
+async fn cleanup_test_data_handler(Extension(pool): Extension<&'static PgPool>) -> ApiResult<()> {
+    let test_user_emails: Vec<(String,)> =
+        sqlx::query_as("SELECT email FROM users where email LIKE '%'||$1;")
+            .bind(google::TEST_USER_SUFFIX)
+            .fetch_all(pool)
+            .await?;
+    let test_user_emails: Vec<String> = test_user_emails
+        .into_iter()
+        .map(|(email,)| email)
+        .filter(|email| email.ends_with(google::TEST_USER_SUFFIX))
+        .filter(|email| {
+            let parts = email.split("-").collect::<Vec<&str>>();
+            let Some(Ok(create_time)) = parts.get(1).map(|t| t.parse::<u64>()) else {
+                return true;
+            };
+            let Ok(d) =
+                SystemTime::now().duration_since(UNIX_EPOCH + Duration::from_millis(create_time))
+            else {
+                return true;
+            };
+            // Enable post hoc debugging and avoid interfering with other running tests
+            // by only deleting users after some time has passed.
+            d > Duration::from_secs(3 * 60 * 60)
+        })
+        .collect::<Vec<String>>();
+    tracing::debug!("Deleting test users{test_user_emails:?}");
+
+    if let Err(e) = sqlx::query(
+        "
+        DELETE FROM projects
+        WHERE project_id IN (
+            SELECT project_id FROM project_permissions
+            WHERE email IN (SELECT * FROM unnest($1))
+        );",
+    )
+    .bind(&test_user_emails)
+    .execute(pool)
+    .await
+    {
+        return Err(internal_error(&format!(
+            "Failed to delete test projects: {e}"
+        )));
+    }
+    if let Err(e) = sqlx::query(
+        "
+        DELETE FROM project_permissions
+        WHERE email IN (SELECT * FROM unnest($1));",
+    )
+    .bind(&test_user_emails)
+    .execute(pool)
+    .await
+    {
+        return Err(internal_error(&format!(
+            "Failed to delete test project_permissions: {e}"
+        )));
+    }
+    if let Err(e) = sqlx::query(
+        "
+        DELETE FROM users
+        WHERE email IN (SELECT * FROM unnest($1));",
+    )
+    .bind(&test_user_emails)
+    .execute(pool)
+    .await
+    {
+        return Err(internal_error(&format!("Failed to delete test users: {e}")));
+    }
+
+    Ok(())
+}
--- frontend/tests/test.ts
+++ frontend/tests/test.ts
@@ -2,53 +2,71 @@ import { expect, request, test, type Page } from "@playwright/test";
 
 test.describe.configure({ mode: "serial" });
 
-let page: Page;
+test.describe.serial("all tests", () => {
+  let page: Page;
 
-test.beforeAll(async ({ browser }) => {
-  page = await browser.newPage();
-});
-
-test("home page presents login header and button", async () => {
-  await page.goto("/");
-  await expect(page.locator("h1")).toHaveText("Koso");
-});
-
-test("log user in and view projects", async () => {
-  await page.goto("/");
-
-  const login_url = `/api/auth/login`;
-  const apiContext = await request.newContext({});
-  // Avoid test cross-talk by logging in with a randomly generated user.
-  const email = `${Math.random().toString(36).slice(2)}-${Date.now()}-test@test.koso.app`;
-  const token = jwt(email);
-  const res = await apiContext.post(login_url, {
-    data: {},
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
+  test.beforeAll(async ({ browser }) => {
+    page = await browser.newPage();
   });
-  expect(res.ok()).toBeTruthy();
 
-  await page.evaluate(
-    ([token]) => window.localStorage.setItem("credential", token),
-    [token],
-  );
+  test.afterAll(async () => {
+    await page.goto("/");
 
-  await page.goto("/projects");
-  await expect(page.getByText("Create your first Koso project!")).toBeVisible();
-});
+    const apiContext = await request.newContext({});
+    const token = jwt(`cleanup-test@test.koso.app`);
+    const res = await apiContext.post("/api/dev/cleanup_test_data", {
+      data: {},
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    expect(res.ok()).toBeTruthy();
+  });
 
-test("create a project and rename it to Integration Test Project", async () => {
-  await page.getByRole("button", { name: "new project" }).click();
+  test("home page presents login header and button", async () => {
+    await page.goto("/");
+    await expect(page.locator("h1")).toHaveText("Koso");
+  });
 
-  await page.getByTestId("set-project-name-button").click();
-  await page.keyboard.press("ControlOrMeta+A");
-  await page.keyboard.type("Integration Test Project");
-  await page.keyboard.press("Enter");
+  test("log user in and view projects", async () => {
+    await page.goto("/");
 
-  await expect(page.getByTestId("set-project-name-button")).toHaveText(
-    "Integration Test Project",
-  );
+    const login_url = `/api/auth/login`;
+    const apiContext = await request.newContext({});
+    // Avoid test cross-talk by logging in with a randomly generated user.
+    const email = `${Math.random().toString(36).slice(2)}-${Date.now()}-test@test.koso.app`;
+    const token = jwt(email);
+    const res = await apiContext.post(login_url, {
+      data: {},
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    expect(res.ok()).toBeTruthy();
+
+    await page.evaluate(
+      ([token]) => window.localStorage.setItem("credential", token),
+      [token],
+    );
+
+    await page.goto("/projects");
+    await expect(
+      page.getByText("Create your first Koso project!"),
+    ).toBeVisible();
+  });
+
+  test("create a project and rename it to Integration Test Project", async () => {
+    await page.getByRole("button", { name: "new project" }).click();
+
+    await page.getByTestId("set-project-name-button").click();
+    await page.keyboard.press("ControlOrMeta+A");
+    await page.keyboard.type("Integration Test Project");
+    await page.keyboard.press("Enter");
+
+    await expect(page.getByTestId("set-project-name-button")).toHaveText(
+      "Integration Test Project",
+    );
+  });
 });
 
 function jwt(email: string) {
