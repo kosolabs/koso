c753410eeed7b2a1df64a1a5c1f4e34426500bfe
Shad Sharma <shadanan@gmail.com>
2024-09-24 14:19:13 -0400

    Support for delay and event bindings

--- frontend/src/lib/toolbar-button.svelte
+++ frontend/src/lib/toolbar-button.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { Button } from "$lib/ui/button";
   import { Tooltip } from "$lib/ui/tooltip";
+  import { cn } from "$lib/utils";
   import type { HTMLButtonAttributes } from "svelte/elements";
 
   type Props = HTMLButtonAttributes & {
@@ -11,9 +12,20 @@
   let { children, title, Icon, class: classes, ...props }: Props = $props();
 </script>
 
-<Tooltip portalEl={document.body}>
-  {#snippet trigger()}
-    <Button class={classes} variant="ghost" {...props}>
+<Tooltip
+  class={cn("rounded bg-primary px-3 py-1 text-sm text-primary-foreground")}
+  portalEl={document.body}
+>
+  {#snippet trigger({ show, hide })}
+    <Button
+      class={classes}
+      variant="ghost"
+      onmouseenter={show}
+      onmouseleave={hide}
+      onfocus={show}
+      onblur={hide}
+      {...props}
+    >
       <Icon class="w-4 sm:me-2" />
       <div class="text-xs max-sm:hidden">{title}</div>
     </Button>
--- frontend/src/lib/ui/tooltip/tooltip.svelte
+++ frontend/src/lib/ui/tooltip/tooltip.svelte
@@ -6,50 +6,53 @@
 
   type Props = {
     children: Snippet;
-    trigger: Snippet;
+    trigger: Snippet<[{ show: () => void; hide: () => void }]>;
     arrow?: boolean;
     visible?: boolean;
+    delay?: number;
     placement?: popper.Placement;
     portalEl?: HTMLElement;
+    class?: string;
   };
 
   let {
     children,
     trigger,
     visible = $bindable(false),
+    delay = 500,
     placement = "top",
     portalEl,
+    class: classes,
   }: Props = $props();
 
   let referenceEl: popper.ReferenceElement | undefined = $state();
   let tooltipEl: popper.FloatingElement | undefined = $state();
+  let tooltipTimeout: number | undefined;
 
   function showTooltip() {
-    visible = true;
+    tooltipTimeout = window.setTimeout(() => (visible = true), delay);
   }
 
   function hideTooltip() {
+    window.clearTimeout(tooltipTimeout);
     visible = false;
   }
 
-  function update() {
+  async function update() {
     if (!referenceEl || !tooltipEl) return;
-    popper
-      .computePosition(referenceEl, tooltipEl, {
-        placement,
-        middleware: [
-          popper.offset(6),
-          popper.flip(),
-          popper.shift({ padding: 5 }),
-        ],
-      })
-      .then(({ x, y }) => {
-        if (!referenceEl || !tooltipEl) return;
-        Object.assign(tooltipEl.style, {
-          left: `${x}px`,
-          top: `${y}px`,
-        });
-      });
+    const { x, y } = await popper.computePosition(referenceEl, tooltipEl, {
+      placement,
+      middleware: [
+        popper.offset(6),
+        popper.flip(),
+        popper.shift({ padding: 5 }),
+      ],
+    });
+    if (!referenceEl || !tooltipEl) return;
+    Object.assign(tooltipEl.style, {
+      left: `${x}px`,
+      top: `${y}px`,
+    });
   }
 
   $effect(() => {
@@ -57,25 +60,16 @@
   });
 </script>
 
-<span
-  role="tooltip"
-  bind:this={referenceEl}
-  onmouseenter={showTooltip}
-  onmouseleave={hideTooltip}
-  onfocus={showTooltip}
-  onblur={hideTooltip}
->
+<div bind:this={referenceEl}>
   {#if trigger}
-    {@render trigger()}
+    {@render trigger({ show: showTooltip, hide: hideTooltip })}
   {/if}
-</span>
+</div>
 
 {#snippet tooltip()}
   {#if visible}
     <div
-      class={cn(
-        "absolute left-0 top-0 z-50 w-max overflow-hidden rounded bg-primary p-1 text-sm text-primary-foreground",
-      )}
+      class={cn("absolute left-0 top-0 z-50 w-max overflow-hidden", classes)}
       role="tooltip"
       transition:flyAndScale
       bind:this={tooltipEl}
