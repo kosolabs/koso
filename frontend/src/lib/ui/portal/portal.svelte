47a982e28fa23ba7628cee76cb142b1806738855
Shad Sharma <shadanan@gmail.com>
2024-09-23 22:00:24 -0700

    Rename stuff

--- frontend/src/lib/ui/portal/portal.svelte
+++ frontend/src/lib/ui/portal/portal.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
   import { mount, unmount, type Snippet } from "svelte";
-  import PortalConsumer from "./portal-consumer.svelte";
+  import Proxy from "./proxy.svelte";
 
   type Props = { children?: Snippet; target?: HTMLElement };
   const { children, target = document.body }: Props = $props();
 
   $effect(() => {
-    const instance = mount(PortalConsumer, { target, props: { children } });
+    const instance = mount(Proxy, { target, props: { children } });
     return () => unmount(instance);
   });
 </script>
