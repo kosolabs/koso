c5aa354acf7445fd0ccba976c7ca315cafc85104
kyle <leonhard.kyle@gmail.com>
2024-09-24 11:53:35 -0700

    Revert "Bump rollup from 4.21.3 to 4.22.4 in /frontend in the npm_and… (#242)
    …_yarn group (#240)"
    
    This reverts commit 9cd9ea70645985f2f6b3238a8496da3661922785.

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -6,6 +6,7 @@ const config: PlaywrightTestConfig = {
     port: process.env.CI ? 3000 : process.env.PW_SERVER_PORT || 5173,
     reuseExistingServer: !process.env.CI,
     stdout: "pipe",
+    timeout: 180000,
   },
   testDir: "tests",
   testMatch: /(.+\.)?(test|spec)\.[jt]s/,
