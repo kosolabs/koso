05f78452445fe75965e3bc2f67d7b2e262e28627
kyle <leonhard.kyle@gmail.com>
2024-09-25 16:08:29 -0700

    Reload the app when a new version is deployed (#250)

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -2,6 +2,15 @@
   import { Toaster } from "$lib/components/ui/sonner";
   import { ModeWatcher } from "mode-watcher";
   import "../app.css";
+  import { updated } from "$app/stores";
+  import { version } from "$app/environment";
+
+  $: if ($updated) {
+    console.log(
+      `Reloading page following version update Prior version was ${version}`,
+    );
+    window.location.reload();
+  }
 </script>
 
 <ModeWatcher />
