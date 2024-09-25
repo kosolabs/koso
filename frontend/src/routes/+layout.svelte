d3a6d50d4d5c092b3c48efa9b6037937a81c11c4
Shad Sharma <shadanan@gmail.com>
2024-09-24 18:05:02 -0700

    Migrate Sonner back to shadcn

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { Toaster } from "$lib/ui/toaster";
+  import { Toaster } from "$lib/components/ui/sonner";
   import { ModeWatcher } from "mode-watcher";
   import "../app.css";
 </script>
