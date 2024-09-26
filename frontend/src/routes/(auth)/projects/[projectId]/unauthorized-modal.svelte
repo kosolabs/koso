c6d816315426b1c9652698fcb2b7816f3139adf8
Shad Sharma <shadanan@gmail.com>
2024-09-25 22:48:05 -0700

    Take Me Home dialog redirects when dismissed

---
+++ frontend/src/routes/(auth)/projects/[projectId]/unauthorized-modal.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import { goto } from "$app/navigation";
+  import { Button } from "$lib/components/ui/button";
+  import * as Dialog from "$lib/components/ui/dialog";
+  import { lastVisitedProjectId } from "$lib/nav";
+
+  type Props = {
+    open: boolean;
+  };
+  let { open = $bindable(false) }: Props = $props();
+
+  async function goHome(open: boolean) {
+    if (!open) {
+      // Don't redirect the user back to a project they don't have access too.
+      $lastVisitedProjectId = null;
+      await goto("/projects");
+    }
+  }
+</script>
+
+<Dialog.Root bind:open onOpenChange={goHome}>
+  <Dialog.Content>
+    <Dialog.Header>
+      <Dialog.Title>Unauthorized</Dialog.Title>
+      <Dialog.Description>
+        You do not have access to the project or the project does not exist.
+      </Dialog.Description>
+    </Dialog.Header>
+    <Dialog.Footer>
+      <Button onclick={() => goHome(false)}>Take me home</Button>
+    </Dialog.Footer>
+  </Dialog.Content>
+</Dialog.Root>
