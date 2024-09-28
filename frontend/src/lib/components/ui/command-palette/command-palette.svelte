08d9edfe97f8e399ebcfe38e138707350770c4a6
Shad Sharma <shadanan@gmail.com>
2024-09-28 12:13:21 +0900

    Introduce the command palette

---
+++ frontend/src/lib/components/ui/command-palette/command-palette.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+  import * as Command from "$lib/components/ui/command/index.js";
+  import Calendar from "svelte-radix/Calendar.svelte";
+  import EnvelopeClosed from "svelte-radix/EnvelopeClosed.svelte";
+  import Face from "svelte-radix/Face.svelte";
+  import Gear from "svelte-radix/Gear.svelte";
+  import Person from "svelte-radix/Person.svelte";
+  import Rocket from "svelte-radix/Rocket.svelte";
+
+  type Props = {
+    open: boolean;
+  };
+  let { open = $bindable(false) }: Props = $props();
+</script>
+
+<Command.Dialog bind:open>
+  <Command.Input placeholder="Type a command or search..." />
+  <Command.List>
+    <Command.Empty>No results found.</Command.Empty>
+    <Command.Group heading="Suggestions">
+      <Command.Item>
+        <Calendar class="mr-2 h-4 w-4" />
+        <span>Calendar</span>
+      </Command.Item>
+      <Command.Item>
+        <Face class="mr-2 h-4 w-4" />
+        <span>Search Emoji</span>
+      </Command.Item>
+      <Command.Item>
+        <Rocket class="mr-2 h-4 w-4" />
+        <span>Launch</span>
+      </Command.Item>
+    </Command.Group>
+    <Command.Separator />
+    <Command.Group heading="Settings">
+      <Command.Item>
+        <Person class="mr-2 h-4 w-4" />
+        <span>Profile</span>
+        <Command.Shortcut>⌘P</Command.Shortcut>
+      </Command.Item>
+      <Command.Item>
+        <EnvelopeClosed class="mr-2 h-4 w-4" />
+        <span>Mail</span>
+        <Command.Shortcut>⌘B</Command.Shortcut>
+      </Command.Item>
+      <Command.Item>
+        <Gear class="mr-2 h-4 w-4" />
+        <span>Settings</span>
+        <Command.Shortcut>⌘S</Command.Shortcut>
+      </Command.Item>
+    </Command.Group>
+  </Command.List>
+</Command.Dialog>
