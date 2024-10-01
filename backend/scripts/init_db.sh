ef417f920cf654362585015ae653769ad07867c2
Kyle Leonhard <leonhard.kyle@gmail.com>
2024-09-30 21:31:37 -0700

    update script

--- backend/scripts/init_db.sh
+++ backend/scripts/init_db.sh
@@ -6,18 +6,6 @@ DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
 DB_NAME="${POSTGRES_DB:=postgres}"
 DB_PORT="${POSTGRES_PORT:=5432}"
 
-# Allow to skip Docker if a dockerized Postgres database is already running
-if [[ -z "${SKIP_DOCKER}" ]]; then
-
-  docker run \
-    -e POSTGRES_USER=${DB_USER} \
-    -e POSTGRES_PASSWORD=${DB_PASSWORD} \
-    -e POSTGRES_DB=${DB_NAME} \
-    -p "${DB_PORT}":5432 \
-    -d postgres:alpine \
-    postgres -N 1000
-fi
-
 # Keep pinging Postgres until it's ready to accept commands
 export PGPASSWORD="${DB_PASSWORD}"
 until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
@@ -28,6 +16,22 @@ done
 echo >&2 "Postgres is up and running on port ${DB_PORT}!"
 export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
 
-sqlx database create
-sqlx migrate run
+# Allow to skip Docker if a dockerized Postgres database is already running
+if [[ -z "${KOSO_IMAGE}" ]]; then
+  sqlx database create
+  sqlx migrate run
+else
+  docker run \
+    --env DATABASE_URL=$DATABASE_URL \
+    --network=host \
+    --rm \
+    $KOSO_IMAGE \
+    "./sqlx" database create
+  docker run \
+    --env DATABASE_URL=$DATABASE_URL \
+    --network=host \
+    --rm \
+    $KOSO_IMAGE \
+    "./sqlx" migrate run
+fi
 echo >&2 "Postgres has been migrated, ready to go!"
