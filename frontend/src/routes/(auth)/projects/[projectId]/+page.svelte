60a95ce5dff3f179be1a113d069bd5d77c5e591f
Kyle Leonhard <leonhard.kyle@gmail.com>
2024-10-04 10:50:19 -0700

    Simplify export

--- frontend/src/routes/(auth)/projects/[projectId]/+page.svelte
+++ frontend/src/routes/(auth)/projects/[projectId]/+page.svelte
@@ -3,7 +3,6 @@
   import { token, user, type User } from "$lib/auth";
   import { Alert } from "$lib/components/ui/alert";
   import { Button } from "$lib/components/ui/button";
-  import * as Dialog from "$lib/components/ui/dialog";
   import { Editable } from "$lib/components/ui/editable";
   import { DagTable } from "$lib/dag-table";
   import { Koso } from "$lib/koso";
@@ -19,6 +18,7 @@
   import { KosoSocket } from "$lib/socket";
   import { FileDown, UserPlus } from "lucide-svelte";
   import { onDestroy, onMount } from "svelte";
+  import { toast } from "svelte-sonner";
   import * as Y from "yjs";
   import ProjectShareModal from "./project-share-modal.svelte";
   import UnauthorizedModal from "./unauthorized-modal.svelte";
@@ -56,15 +56,14 @@
     }
   }
 
-  let showProjectExport: boolean = false;
-  let downloadProjectExport: string | null = null;
-  async function doExportProject() {
+  async function exportProjectToClipboard() {
     if (!$user || !$token) throw new Error("User is unauthorized");
-    showProjectExport = true;
 
-    let projectExport = await exportProject($token, projectId);
-    downloadProjectExport = JSON.stringify(projectExport);
+    toast.info("Exporting project...");
+    const projectExport = await exportProject($token, projectId);
+    const downloadProjectExport = JSON.stringify(projectExport, null, 2);
     navigator.clipboard.writeText(downloadProjectExport);
+    toast.info("Project exported to clipboard.");
   }
 
   let showSocketOfflineAlert: boolean = false;
@@ -118,7 +117,7 @@
     </div>
   </svelte:fragment>
   <svelte:fragment slot="right-items">
-    <Button title="Export Project" onclick={doExportProject}>
+    <Button title="Export Project" onclick={exportProjectToClipboard}>
       <FileDown />
     </Button>
     <Button
@@ -137,24 +136,6 @@
     <Alert>Connection to server lost. Working offline.</Alert>
   </div>
 {/if}
-
-<Dialog.Root bind:open={showProjectExport}>
-  <Dialog.Content>
-    <Dialog.Header>
-      <Dialog.Title>Export</Dialog.Title>
-    </Dialog.Header>
-
-    {#if downloadProjectExport}
-      <div>Copied to Clipboard</div>
-      <div>
-        <textarea readonly class="h-48 w-full">{downloadProjectExport}</textarea
-        >
-      </div>
-    {:else}
-      <div>Loading...</div>
-    {/if}
-  </Dialog.Content>
-</Dialog.Root>
 
 <UnauthorizedModal bind:open={showUnauthorizedModal} />
 <ProjectShareModal bind:open={openShareModal} bind:projectUsers {project} />
