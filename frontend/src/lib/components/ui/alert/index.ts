4aaef484ebbf33622c2ab538d8a23efed64a82b6
Shad Sharma <shadanan@gmail.com>
2024-10-16 15:26:16 -0700

    Make dark mode destructive text more visible

--- frontend/src/lib/components/ui/alert/index.ts
+++ frontend/src/lib/components/ui/alert/index.ts
@@ -1,8 +1,8 @@
 import { type VariantProps, tv } from "tailwind-variants";
 
-import Root from "./alert.svelte";
 import Description from "./alert-description.svelte";
 import Title from "./alert-title.svelte";
+import Root from "./alert.svelte";
 
 export const alertVariants = tv({
   base: "[&>svg]:text-foreground relative w-full rounded-lg border px-4 py-3 text-sm [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg~*]:pl-7",
@@ -10,7 +10,7 @@ export const alertVariants = tv({
     variant: {
       default: "bg-background text-foreground",
       destructive:
-        "border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive",
+        "border-destructive text-destructive [&>svg]:text-destructive",
     },
   },
   defaultVariants: {
@@ -22,11 +22,11 @@ export type Variant = VariantProps<typeof alertVariants>["variant"];
 export type HeadingLevel = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 
 export {
-  Root,
-  Description,
-  Title,
   //
   Root as Alert,
   Description as AlertDescription,
   Title as AlertTitle,
+  Description,
+  Root,
+  Title,
 };
