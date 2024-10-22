c610998eb71e3f860810532a1152237f9977dec5
Shad Sharma <shadanan@gmail.com>
2024-10-22 06:01:26 +0100

    Configure /api and !/api routes

--- frontend/src/service-worker.ts
+++ frontend/src/service-worker.ts
@@ -6,20 +6,30 @@
 import { build, files, prerendered, version } from "$service-worker";
 import { cleanupOutdatedCaches, precacheAndRoute } from "workbox-precaching";
 import { registerRoute } from "workbox-routing";
-import { StaleWhileRevalidate } from "workbox-strategies";
+import { NetworkFirst, StaleWhileRevalidate } from "workbox-strategies";
 
 const precache_list = [...build, ...files, ...prerendered].map((s) => ({
   url: s,
   revision: version,
 }));
 
-console.debug("Precached: ", precache_list);
+console.debug("Precached:", precache_list);
 
 precacheAndRoute(precache_list);
 
 cleanupOutdatedCaches();
 
+// Cache user avatar images
 registerRoute(
-  ({ url }) => url.pathname.startsWith("/projects"),
+  ({ url }) => url.hostname.endsWith("googleusercontent.com"),
   new StaleWhileRevalidate(),
 );
+
+// Anything that is not /api can serve from the cache first
+registerRoute(
+  ({ url }) => !url.pathname.startsWith("/api"),
+  new StaleWhileRevalidate(),
+);
+
+// Serve requests to /api from the network first, and from the cache if offline.
+registerRoute(({ url }) => url.pathname.startsWith("/api"), new NetworkFirst());
