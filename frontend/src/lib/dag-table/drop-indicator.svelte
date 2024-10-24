4739a1ac6c766b04df8f08de652a0b575acab785
Shad Sharma <shadanan@gmail.com>
2024-10-24 15:33:50 -0700

    Make drop indicator tooltip prettier and more responsive (#405)

---
+++ frontend/src/lib/dag-table/drop-indicator.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+  import * as Tooltip from "$lib/components/ui/tooltip";
+  import type { Task } from "$lib/koso";
+  import { cn } from "$lib/utils";
+  import { ArrowBigDown } from "lucide-svelte";
+
+  type Props = {
+    src: Task;
+    dest: Task;
+    width: number;
+    offset: number;
+    type: "Peer" | "Child";
+  };
+  const { src, dest, width, offset, type }: Props = $props();
+
+  let open = $state(false);
+
+  $effect(() => {
+    const timeout = window.setTimeout(() => (open = true), 500);
+    return () => clearTimeout(timeout);
+  });
+</script>
+
+<Tooltip.Root {open}>
+  <Tooltip.Trigger asChild let:builder>
+    <button
+      use:builder.action
+      {...builder}
+      class={cn(
+        "absolute -my-[0.1rem] h-1 rounded",
+        type === "Peer" ? "bg-primary" : "bg-secondary",
+      )}
+      style="width: {width}px; margin-left: {offset}px;"
+      aria-label={`Task ${dest.num} ${type} Drop Indicator`}
+    ></button>
+  </Tooltip.Trigger>
+  <Tooltip.Content
+    class={cn(
+      type === "Peer"
+        ? "bg-primary text-primary-foreground"
+        : "bg-secondary text-secondary-foreground",
+    )}
+  >
+    <Tooltip.Arrow class="z-50" />
+    <div class="flex flex-col items-center text-sm">
+      <div class="flex items-center gap-1">
+        <span>Task</span>
+        <span class="font-bold">{src.num}</span>
+      </div>
+      <ArrowBigDown size={16} />
+      <div class="flex items-center gap-1">
+        <span class="font-bold">{type}</span>
+        <span>of task</span>
+        <span class="font-bold">{dest.num}</span>
+      </div>
+    </div>
+  </Tooltip.Content>
+</Tooltip.Root>
