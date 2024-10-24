2bbca33534ee22b1e7408fce9ef43660597da498
Shad Sharma <shadanan@gmail.com>
2024-10-24 11:07:20 -0700

    Don't do button animation on links (#399)

--- frontend/src/lib/components/ui/button/index.ts
+++ frontend/src/lib/components/ui/button/index.ts
@@ -6,14 +6,16 @@ const buttonVariants = tv({
   base: "focus-visible:ring-ring inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50",
   variants: {
     variant: {
-      default: "bg-primary text-primary-foreground hover:bg-primary/90 shadow",
+      default:
+        "bg-primary text-primary-foreground hover:bg-primary/90 shadow transition-all active:scale-95",
       destructive:
-        "bg-destructive text-destructive-foreground hover:bg-destructive/90 shadow-sm",
+        "bg-destructive text-destructive-foreground hover:bg-destructive/90 shadow-sm transition-all active:scale-95",
       outline:
-        "border-input bg-background hover:bg-accent hover:text-accent-foreground border shadow-sm",
+        "border-input bg-background hover:bg-accent hover:text-accent-foreground border shadow-sm transition-all active:scale-95",
       secondary:
-        "bg-secondary text-secondary-foreground hover:bg-secondary/80 shadow-sm",
-      ghost: "hover:bg-accent hover:text-accent-foreground",
+        "bg-secondary text-secondary-foreground hover:bg-secondary/80 shadow-sm transition-all active:scale-95",
+      ghost:
+        "hover:bg-accent hover:text-accent-foreground transition-all active:scale-95",
       link: "text-primary underline-offset-4 hover:underline",
     },
     size: {
@@ -40,12 +42,12 @@ type Props = ButtonPrimitive.Props & {
 type Events = ButtonPrimitive.Events;
 
 export {
-  Root,
-  type Props,
-  type Events,
   //
   Root as Button,
-  type Props as ButtonProps,
-  type Events as ButtonEvents,
+  Root,
   buttonVariants,
+  type Events as ButtonEvents,
+  type Props as ButtonProps,
+  type Events,
+  type Props,
 };
