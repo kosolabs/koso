e46cb0d6a13301e4ecda3b034d8baafb0214ee84
Kyle Leonhard <leonhard.kyle@gmail.com>
2024-10-27 10:12:18 -0700

    Handle structured errors in the frontend to show nice messages

---
+++ frontend/src/lib/api.ts
@@ -0,0 +1,62 @@
+import { logout_on_authentication_error } from "./errors";
+
+export type ErrorResponseBody = {
+  status: number;
+  reason: string;
+  msg: string;
+};
+
+export class KosoError extends Error {
+  // Status code in number form. e.g. 400, 500
+  status: number;
+  // Terse, stable, machine readable error reason.
+  // e.g. NO_STOCK
+  reason: string;
+
+  constructor({
+    status,
+    reason,
+    msg,
+  }: {
+    status: number;
+    reason: string;
+    msg: string;
+  }) {
+    super(`${reason} (${status}: ${msg})`);
+    this.status = status;
+    this.reason = reason;
+  }
+}
+
+/**
+ * Returns the response body as json or throws if the response is not OK.
+ * @throws {KosoError}
+ */
+export async function parse_response<T>(response: Response): Promise<T> {
+  if (response.ok) {
+    const ret: T = await response.json();
+    return ret;
+  }
+
+  logout_on_authentication_error(response);
+  if (response.headers.get("Content-Type") === "application/json") {
+    const error: ErrorResponseBody = await response.json();
+    if (error.status !== response.status) {
+      console.warn(
+        `Error status (${error.status}) does not match response status ${response.status}`,
+        error,
+        response,
+      );
+    }
+    throw new KosoError({
+      status: error.status,
+      reason: error.reason,
+      msg: error.msg,
+    });
+  }
+  throw new KosoError({
+    status: response.status,
+    reason: "UNKNOWN",
+    msg: "No error details present.",
+  });
+}
