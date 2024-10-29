9e0339820db9e4e92b28785813374669e2ed26da
Shad Sharma <shadanan@gmail.com>
2024-10-29 14:42:45 -0700

    Implement filtering and sorting

--- frontend/src/lib/dag-table/link-or-move-command.svelte
+++ frontend/src/lib/dag-table/link-or-move-command.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
   import * as Command from "$lib/components/ui/command";
   import * as Popover from "$lib/components/ui/popover";
+  import type { Koso } from "$lib/koso.svelte";
   import { Shortcut } from "$lib/shortcuts";
-  import type { Snippet } from "svelte";
+  import { getContext, type Snippet } from "svelte";
 
   type Props = {
     children: Snippet;
@@ -10,11 +11,27 @@
     closeFocus: HTMLElement;
   };
   let { children, visible = $bindable(false), closeFocus }: Props = $props();
+
+  const koso = getContext<Koso>("koso");
+
+  let tasks = Object.values(koso.toJSON());
+  let query = $state("");
+  let results = $derived(
+    tasks
+      .filter(
+        (task) =>
+          task.id !== "root" &&
+          (task.name.toLocaleLowerCase().includes(query.toLocaleLowerCase()) ||
+            task.num.startsWith(query)),
+      )
+      .sort((t1, t2) => t2.children.length - t1.children.length),
+  );
 </script>
 
 <Popover.Root bind:open={visible} {closeFocus} portal={null}>
   <Popover.Trigger>{@render children()}</Popover.Trigger>
   <Popover.Content
+    class="w-auto max-w-2xl"
     onkeydown={(event) => {
       event.stopPropagation();
       if (Shortcut.CANCEL.matches(event)) {
@@ -22,21 +39,16 @@
       }
     }}
   >
-    <Command.Root>
-      <Command.Input placeholder="Type a command or search..." />
+    <Command.Root shouldFilter={false}>
+      <Command.Input
+        placeholder="Type a task name or number to search..."
+        bind:value={query}
+      />
       <Command.List>
         <Command.Empty>No results found.</Command.Empty>
-        <Command.Group heading="Suggestions">
-          <Command.Item>Calendar</Command.Item>
-          <Command.Item>Search Emoji</Command.Item>
-          <Command.Item>Calculator</Command.Item>
-        </Command.Group>
-        <Command.Separator />
-        <Command.Group heading="Settings">
-          <Command.Item>Profile</Command.Item>
-          <Command.Item>Billing</Command.Item>
-          <Command.Item>Settings</Command.Item>
-        </Command.Group>
+        {#each results as result (result.id)}
+          <Command.Item>{result.name}</Command.Item>
+        {/each}
       </Command.List>
     </Command.Root>
   </Popover.Content>
