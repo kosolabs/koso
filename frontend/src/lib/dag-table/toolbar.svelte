261e913807d0a2ca351f5d6d206e732e2fc3a40d
Kyle Leonhard <leonhard.kyle@gmail.com>
2024-10-30 14:42:30 -0700

    Avoid toolbar vertical overflows

--- frontend/src/lib/dag-table/toolbar.svelte
+++ frontend/src/lib/dag-table/toolbar.svelte
@@ -16,7 +16,7 @@
 <div
   class={cn(
     "z-10 flex items-center overflow-x-scroll px-2 backdrop-blur-sm",
-    "fixed bottom-0 left-0 h-12 w-full border-t",
+    "fixed bottom-0 left-0 min-h-12 w-full border-t",
     "sm:sticky sm:top-0 sm:gap-2 sm:border-b",
   )}
 >
