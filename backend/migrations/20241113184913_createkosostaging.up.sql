9f91c991e8bb4e4b34510a8415987cdb4ee77201
Kyle Leonhard <leonhard.kyle@gmail.com>
2024-11-13 17:30:40 -0800

    Only setup permissions

--- backend/migrations/20241113184913_createkosostaging.up.sql
+++ backend/migrations/20241113184913_createkosostaging.up.sql
@@ -8,10 +8,3 @@ INSERT INTO project_permissions
 VALUES
   ('koso-staging', 'leonhard.kyle@gmail.com'),
   ('koso-staging', 'shadanan@gmail.com');
-
-INSERT INTO users
-  (email, name, picture)
-VALUES
-  ('leonhard.kyle@gmail.com', 'Kyle Leonhard', 'https://lh3.googleusercontent.com/a/ACg8ocIIqNHG-bPON1NKXNOCiJR8fCS_ze3iIAsCvunJ4_kyhKJXFA=s96-c'),
-  ('shadanan@gmail.com', 'Shad Sharma', 'https://lh3.googleusercontent.com/a/ACg8ocIRfl1MJrdKF_V8e46SQijmFzs1JoEaQLogCsOEIYC-T2Hk2xcPKw=s96-c')
-ON CONFLICT DO NOTHING;
