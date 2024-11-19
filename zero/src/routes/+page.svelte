703c5ea32926f9fb34f83701211f3c74b38667d4
Shad Sharma <shadanan@gmail.com>
2024-11-19 14:13:49 -0800

    Add shadcn-svelte (#551)

--- zero/src/routes/+page.svelte
+++ zero/src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { Button } from "$lib/components/ui/button";
+  import { Input } from "$lib/components/ui/input";
   import { invoke } from "@tauri-apps/api/core";
 
   let name = $state("");
@@ -10,115 +12,17 @@
   }
 </script>
 
-<main class="container">
-  <h1>Welcome to Koso Zero</h1>
+<main class="flex flex-col gap-16 items-center mt-auto p-20">
+  <h1 class="text-4xl">Welcome to Koso Zero</h1>
 
-  <div class="row">
-    <img src="/logo.svg" class="logo koso" alt="Koso Logo" />
+  <img src="/logo.svg" class="w-20" alt="Koso Logo" />
+
+  <div class="flex gap-2">
+    <Input bind:value={name} placeholder="Enter a name..." />
+    <Button onclick={greet}>Greet</Button>
   </div>
-
-  <form class="row" onsubmit={greet}>
-    <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
-    <button type="submit">Greet</button>
-  </form>
-  <p>{greetMsg}</p>
+  <p class="text-2xl">{greetMsg}</p>
 </main>
 
 <style>
-.logo.koso:hover {
-  filter: drop-shadow(0 0 2em #747bff);
-}
-
-:root {
-  font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
-  font-size: 16px;
-  line-height: 24px;
-  font-weight: 400;
-
-  color: #0f0f0f;
-  background-color: #f6f6f6;
-
-  font-synthesis: none;
-  text-rendering: optimizeLegibility;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-text-size-adjust: 100%;
-}
-
-.container {
-  margin: 0;
-  padding-top: 10vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  text-align: center;
-}
-
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: 0.75s;
-}
-
-.row {
-  display: flex;
-  justify-content: center;
-}
-
-h1 {
-  text-align: center;
-}
-
-input,
-button {
-  border-radius: 8px;
-  border: 1px solid transparent;
-  padding: 0.6em 1.2em;
-  font-size: 1em;
-  font-weight: 500;
-  font-family: inherit;
-  color: #0f0f0f;
-  background-color: #ffffff;
-  transition: border-color 0.25s;
-  box-shadow: 0 2px 2px rgba(0, 0, 0, 0.2);
-}
-
-button {
-  cursor: pointer;
-}
-
-button:hover {
-  border-color: #396cd8;
-}
-button:active {
-  border-color: #396cd8;
-  background-color: #e8e8e8;
-}
-
-input,
-button {
-  outline: none;
-}
-
-#greet-input {
-  margin-right: 5px;
-}
-
-@media (prefers-color-scheme: dark) {
-  :root {
-    color: #f6f6f6;
-    background-color: #2f2f2f;
-  }
-
-  input,
-  button {
-    color: #ffffff;
-    background-color: #0f0f0f98;
-  }
-  button:active {
-    background-color: #0f0f0f69;
-  }
-}
-
 </style>
