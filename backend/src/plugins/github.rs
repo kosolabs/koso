10854e25c840e6e2adeec473c0548123387a5204
Kyle Leonhard <leonhard.kyle@gmail.com>
2024-11-21 19:41:55 -0800

    ref

--- backend/src/plugins/github.rs
+++ backend/src/plugins/github.rs
@@ -1,19 +1,8 @@
-use std::path::Path;
-
-use anyhow::{anyhow, Result};
-use kosolib::{AppGithub, InstallationRef};
-
-const DEFAULT_SECRETS_DIR: &str = "../.secrets";
+use anyhow::Result;
+use kosolib::{AppGithub, AppGithubConfig, InstallationRef};
 
 pub async fn shad() -> Result<String> {
-    let key_path = Path::new(
-        &std::env::var("SECRETS_DIR").unwrap_or_else(|_| DEFAULT_SECRETS_DIR.to_string()),
-    )
-    .join("github/key.pem")
-    .into_os_string()
-    .into_string()
-    .map_err(|e| anyhow!("Path error: {e:?}"))?;
-    let client = AppGithub::new(&key_path).await?;
+    let client = AppGithub::new(&AppGithubConfig::default()).await?;
     let client = client
         .installation_github(InstallationRef::Org { owner: "kosolabs" })
         .await?;
