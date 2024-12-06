96cf7f16b2805b0a38fd023dd5c0871a607a5109
Kyle Leonhard <leonhard.kyle@gmail.com>
2024-12-06 11:26:48 -0800

    Add settings page with ability to clear local state

---
+++ frontend/src/routes/(auth)/projects/[projectId]/settings/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import Navbar from "$lib/navbar.svelte";
+  import { Button } from "$lib/components/ui/button";
+  import { CREDENTIAL_KEY } from "$lib/auth.svelte";
+  import { toast } from "svelte-sonner";
+
+  async function clearLocalState() {
+    [...Array(localStorage.length).keys()]
+      .map((i) => localStorage.key(i))
+      .forEach((key) => {
+        if (key && key !== CREDENTIAL_KEY) {
+          console.debug(`Removing local storage item '${key}'`);
+          localStorage.removeItem(key);
+        }
+      });
+    sessionStorage.clear();
+    const dbs = await window.indexedDB.databases();
+    for (const db of dbs) {
+      if (db.name) {
+        console.debug(`Deleting IndexedDB: '${db.name}'`);
+        window.indexedDB.deleteDatabase(db.name);
+      }
+    }
+
+    toast.info("Local state cleared!");
+  }
+</script>
+
+<Navbar />
+
+<div class="p-4">
+  <Button onclick={clearLocalState}>Clear local state</Button>
+</div>
