f82c2eb0e3df525d29a2402f8819288d80013b18
Kyle Leonhard <leonhard.kyle@gmail.com>
2024-12-09 15:36:20 -0800

    revert-test

--- frontend/src/lib/dag-table/koso.svelte.ts
+++ frontend/src/lib/dag-table/koso.svelte.ts
@@ -1316,7 +1316,7 @@ export class Koso {
     this.doc.transact(() => {
       this.upsert({
         id: taskId,
-        num: "24",
+        num: this.newNum(),
         name: name,
         children: [],
         reporter: user.email,
