4cf1e311ca37bacfd5f28a09d21ede50141c0292
Shad Sharma <shadanan@gmail.com>
2024-12-09 11:52:53 -0800

    Migrate Button and Tooltip to shadcn-svelte@next (#634)
    - **Migrate button to shadcn-svelte@next**
    - **Migrate tooltip to shadcn-svelte@next**
    - **Migrate toolbar-button to shadcn-svelte@next**
    - **Migrate drop-indicator to shadcn-svelte@next**

--- frontend/src/lib/dag-table/drop-indicator.svelte
+++ frontend/src/lib/dag-table/drop-indicator.svelte
@@ -21,38 +21,48 @@
   });
 </script>
 
-<Tooltip.Root {open}>
-  <Tooltip.Trigger asChild let:builder>
-    <button
-      use:builder.action
-      {...builder}
-      class={cn(
-        "absolute -my-[0.1rem] h-1 rounded",
-        type === "Peer" ? "bg-primary" : "bg-secondary",
-      )}
-      style="width: {width}px; margin-left: {offset}px;"
-      aria-label={`Task ${dest.num} ${type} Drop Indicator`}
-    ></button>
-  </Tooltip.Trigger>
-  <Tooltip.Content
-    class={cn(
-      type === "Peer"
-        ? "bg-primary text-primary-foreground"
-        : "bg-secondary text-secondary-foreground",
-    )}
-  >
-    <Tooltip.Arrow class="z-50" />
-    <div class="flex flex-col items-center text-sm">
-      <div class="flex items-center gap-1">
-        <span>Task</span>
-        <span class="font-bold">{src.num}</span>
-      </div>
-      <ArrowBigDown size={16} />
-      <div class="flex items-center gap-1">
-        <span class="font-bold">{type}</span>
-        <span>of task</span>
-        <span class="font-bold">{dest.num}</span>
-      </div>
-    </div>
-  </Tooltip.Content>
-</Tooltip.Root>
+<Tooltip.Provider>
+  <Tooltip.Root {open}>
+    <Tooltip.Trigger>
+      {#snippet child({ props })}
+        <button
+          {...props}
+          class={cn(
+            "absolute -my-[0.1rem] h-1 rounded",
+            type === "Peer" ? "bg-primary" : "bg-secondary",
+          )}
+          style="width: {width}px; margin-left: {offset}px;"
+          aria-label={`Task ${dest.num} ${type} Drop Indicator`}
+        ></button>
+      {/snippet}
+    </Tooltip.Trigger>
+    <Tooltip.Portal>
+      <Tooltip.Content
+        class={cn(
+          type === "Peer"
+            ? "bg-primary text-primary-foreground"
+            : "bg-secondary text-secondary-foreground",
+        )}
+      >
+        <Tooltip.Arrow
+          class={cn(
+            "z-50",
+            type === "Peer" ? "text-primary" : "text-secondary",
+          )}
+        />
+        <div class="flex flex-col items-center text-sm">
+          <div class="flex items-center gap-1">
+            <span>Task</span>
+            <span class="font-bold">{src.num}</span>
+          </div>
+          <ArrowBigDown size={16} />
+          <div class="flex items-center gap-1">
+            <span class="font-bold">{type}</span>
+            <span>of task</span>
+            <span class="font-bold">{dest.num}</span>
+          </div>
+        </div>
+      </Tooltip.Content>
+    </Tooltip.Portal>
+  </Tooltip.Root>
+</Tooltip.Provider>
