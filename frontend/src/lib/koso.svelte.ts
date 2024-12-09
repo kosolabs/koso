e4e5ff704f1b3871ea5a193cdc97e7bd605a7ee4
Kyle Leonhard <leonhard.kyle@gmail.com>
2024-12-09 14:24:39 -0800

    Rewrite task nums

--- frontend/src/lib/koso.svelte.ts
+++ frontend/src/lib/koso.svelte.ts
@@ -1211,7 +1211,7 @@ export class Koso {
     this.doc.transact(() => {
       this.upsert({
         id: taskId,
-        num: this.newNum(),
+        num: "24",
         name: name,
         children: [],
         reporter: user.email,
