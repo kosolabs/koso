1c0089bcd32448e130c74510ac4eb27cb9dea79f
Shad Sharma <shadanan@gmail.com>
2024-12-09 11:02:20 -0800

    Migrate Tooltip to shadcn-svelte@next

--- frontend/src/lib/components/ui/tooltip/index.ts
+++ frontend/src/lib/components/ui/tooltip/index.ts
@@ -1,18 +1,24 @@
-import { Tooltip as TooltipPrimitive } from "bits-ui0";
+import { Tooltip as TooltipPrimitive } from "bits-ui";
 import Arrow from "./tooltip-arrow.svelte";
 import Content from "./tooltip-content.svelte";
 
 const Root = TooltipPrimitive.Root;
 const Trigger = TooltipPrimitive.Trigger;
+const Provider = TooltipPrimitive.Provider;
+const Portal = TooltipPrimitive.Portal;
 
 export {
   Root,
   Trigger,
-  Content,
   Arrow,
+  Content,
+  Provider,
+  Portal,
   //
   Root as Tooltip,
+  Arrow as TooltipArrow,
   Content as TooltipContent,
   Trigger as TooltipTrigger,
-  Arrow as TooltipArrow,
+  Provider as TooltipProvider,
+  Portal as TooltipPortal,
 };
--- frontend/src/lib/components/ui/tooltip/tooltip-arrow.svelte
+++ frontend/src/lib/components/ui/tooltip/tooltip-arrow.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
   import { cn } from "$lib/utils";
-  import { Tooltip as TooltipPrimitive } from "bits-ui0";
+  import { Tooltip as TooltipPrimitive } from "bits-ui";
 
   const { class: classes, ...props }: TooltipPrimitive.ArrowProps = $props();
 </script>
 
-<TooltipPrimitive.Arrow class={cn("bg-primary", classes)} {...props} />
+<TooltipPrimitive.Arrow class={cn("text-primary", classes)} {...props} />
