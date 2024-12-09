ebdac8ef03a1ef94af38c122bf05d4c5137ff731
Shad Sharma <shadanan@gmail.com>
2024-12-09 13:12:46 -0800

    Migrate Alert to shadcn-svelte@next (#636)

--- frontend/src/lib/components/ui/alert/index.ts
+++ frontend/src/lib/components/ui/alert/index.ts
@@ -1,32 +1,14 @@
-import { type VariantProps, tv } from "tailwind-variants";
-
+import Root from "./alert.svelte";
 import Description from "./alert-description.svelte";
 import Title from "./alert-title.svelte";
-import Root from "./alert.svelte";
-
-export const alertVariants = tv({
-  base: "[&>svg]:text-foreground relative w-full rounded-lg border px-4 py-3 text-sm [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg~*]:pl-7",
-  variants: {
-    variant: {
-      default: "bg-background text-foreground",
-      destructive:
-        "border-destructive text-destructive [&>svg]:text-destructive",
-    },
-  },
-  defaultVariants: {
-    variant: "default",
-  },
-});
-
-export type Variant = VariantProps<typeof alertVariants>["variant"];
-export type HeadingLevel = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+export { alertVariants, type AlertVariant } from "./alert.svelte";
 
 export {
+  Root,
+  Description,
+  Title,
   //
   Root as Alert,
   Description as AlertDescription,
   Title as AlertTitle,
-  Description,
-  Root,
-  Title,
 };
