6e335d0931070dcc977a778138e72322aa5659fa
Kyle Leonhard <leonhard.kyle@gmail.com>
2025-02-05 14:29:52 -0800

    title

--- frontend/src/lib/components/ui/task-action/task-action.svelte
+++ frontend/src/lib/components/ui/task-action/task-action.svelte
@@ -51,15 +51,6 @@
     koso.setTaskStatus(node, status, auth.user);
   }
 
-  export function showDoneConfetti() {
-    confetti.add(getStatusPosition());
-  }
-
-  function getStatusPosition(): DOMRect {
-    if (!statusElement) throw new Error("Status element is undefined");
-    return statusElement.getBoundingClientRect();
-  }
-
   function triggerTitle() {
     if (!rollupProgress) {
       return `${task.status || "Not Started"}${task.statusTime ? ` - ${new Date(task.statusTime).toLocaleString()}` : ""}`;
@@ -67,6 +58,21 @@
     return `${rollupProgress.done} of ${rollupProgress.total} (${Math.round(
       (rollupProgress.done * 100) / rollupProgress.total,
     )}%)`;
+  }
+
+  /**
+   * Triggers the confetti animation when a task's status set to Done. The
+   * confetti's position is relative to the dropdown trigger in this component.
+   * Thus, this function is exported for use in other flows that modify task
+   * status.
+   */
+  export function showDoneConfetti() {
+    confetti.add(getStatusPosition());
+  }
+
+  function getStatusPosition(): DOMRect {
+    if (!statusElement) throw new Error("Status element is undefined");
+    return statusElement.getBoundingClientRect();
   }
 </script>
 
