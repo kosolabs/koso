7878608c9697afe87eb6c9c99e3561399ba4a955
Shad Sharma <shadanan@gmail.com>
2025-02-07 07:13:40 +0000

    Add tailwind styles that implement Material 3

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -59,6 +59,92 @@ const config: Config = {
           border: "hsl(var(--sidebar-border))",
           ring: "hsl(var(--sidebar-ring))",
         },
+        "md-primary": "rgb(var(--md-sys-color-primary) / <alpha-value>)",
+        "md-surface-tint":
+          "rgb(var(--md-sys-color-surface-tint) / <alpha-value>)",
+        "md-on-primary": "rgb(var(--md-sys-color-on-primary) / <alpha-value>)",
+        "md-primary-container":
+          "rgb(var(--md-sys-color-primary-container) / <alpha-value>)",
+        "md-on-primary-container":
+          "rgb(var(--md-sys-color-on-primary-container) / <alpha-value>)",
+        "md-secondary": "rgb(var(--md-sys-color-secondary) / <alpha-value>)",
+        "md-on-secondary":
+          "rgb(var(--md-sys-color-on-secondary) / <alpha-value>)",
+        "md-secondary-container":
+          "rgb(var(--md-sys-color-secondary-container) / <alpha-value>)",
+        "md-on-secondary-container":
+          "rgb(var(--md-sys-color-on-secondary-container) / <alpha-value>)",
+        "md-tertiary": "rgb(var(--md-sys-color-tertiary) / <alpha-value>)",
+        "md-on-tertiary":
+          "rgb(var(--md-sys-color-on-tertiary) / <alpha-value>)",
+        "md-tertiary-container":
+          "rgb(var(--md-sys-color-tertiary-container) / <alpha-value>)",
+        "md-on-tertiary-container":
+          "rgb(var(--md-sys-color-on-tertiary-container) / <alpha-value>)",
+        "md-error": "rgb(var(--md-sys-color-error) / <alpha-value>)",
+        "md-on-error": "rgb(var(--md-sys-color-on-error) / <alpha-value>)",
+        "md-error-container":
+          "rgb(var(--md-sys-color-error-container) / <alpha-value>)",
+        "md-on-error-container":
+          "rgb(var(--md-sys-color-on-error-container) / <alpha-value>)",
+        "md-background": "rgb(var(--md-sys-color-background) / <alpha-value>)",
+        "md-on-background":
+          "rgb(var(--md-sys-color-on-background) / <alpha-value>)",
+        "md-surface": "rgb(var(--md-sys-color-surface) / <alpha-value>)",
+        "md-on-surface": "rgb(var(--md-sys-color-on-surface) / <alpha-value>)",
+        "md-surface-variant":
+          "rgb(var(--md-sys-color-surface-variant) / <alpha-value>)",
+        "md-on-surface-variant":
+          "rgb(var(--md-sys-color-on-surface-variant) / <alpha-value>)",
+        "md-outline": "rgb(var(--md-sys-color-outline) / <alpha-value>)",
+        "md-outline-variant":
+          "rgb(var(--md-sys-color-outline-variant) / <alpha-value>)",
+        "md-shadow": "rgb(var(--md-sys-color-shadow) / <alpha-value>)",
+        "md-scrim": "rgb(var(--md-sys-color-scrim) / <alpha-value>)",
+        "md-inverse-surface":
+          "rgb(var(--md-sys-color-inverse-surface) / <alpha-value>)",
+        "md-inverse-on-surface":
+          "rgb(var(--md-sys-color-inverse-on-surface) / <alpha-value>)",
+        "md-inverse-primary":
+          "rgb(var(--md-sys-color-inverse-primary) / <alpha-value>)",
+        "md-primary-fixed":
+          "rgb(var(--md-sys-color-primary-fixed) / <alpha-value>)",
+        "md-on-primary-fixed":
+          "rgb(var(--md-sys-color-on-primary-fixed) / <alpha-value>)",
+        "md-primary-fixed-dim":
+          "rgb(var(--md-sys-color-primary-fixed-dim) / <alpha-value>)",
+        "md-on-primary-fixed-variant":
+          "rgb(var(--md-sys-color-on-primary-fixed-variant) / <alpha-value>)",
+        "md-secondary-fixed":
+          "rgb(var(--md-sys-color-secondary-fixed) / <alpha-value>)",
+        "md-on-secondary-fixed":
+          "rgb(var(--md-sys-color-on-secondary-fixed) / <alpha-value>)",
+        "md-secondary-fixed-dim":
+          "rgb(var(--md-sys-color-secondary-fixed-dim) / <alpha-value>)",
+        "md-on-secondary-fixed-variant":
+          "rgb(var(--md-sys-color-on-secondary-fixed-variant) / <alpha-value>)",
+        "md-tertiary-fixed":
+          "rgb(var(--md-sys-color-tertiary-fixed) / <alpha-value>)",
+        "md-on-tertiary-fixed":
+          "rgb(var(--md-sys-color-on-tertiary-fixed) / <alpha-value>)",
+        "md-tertiary-fixed-dim":
+          "rgb(var(--md-sys-color-tertiary-fixed-dim) / <alpha-value>)",
+        "md-on-tertiary-fixed-variant":
+          "rgb(var(--md-sys-color-on-tertiary-fixed-variant) / <alpha-value>)",
+        "md-surface-dim":
+          "rgb(var(--md-sys-color-surface-dim) / <alpha-value>)",
+        "md-surface-bright":
+          "rgb(var(--md-sys-color-surface-bright) / <alpha-value>)",
+        "md-surface-container-lowest":
+          "rgb(var(--md-sys-color-surface-container-lowest) / <alpha-value>)",
+        "md-surface-container-low":
+          "rgb(var(--md-sys-color-surface-container-low) / <alpha-value>)",
+        "md-surface-container":
+          "rgb(var(--md-sys-color-surface-container) / <alpha-value>)",
+        "md-surface-container-high":
+          "rgb(var(--md-sys-color-surface-container-high) / <alpha-value>)",
+        "md-surface-container-highest":
+          "rgb(var(--md-sys-color-surface-container-highest) / <alpha-value>)",
       },
       borderRadius: {
         xl: "calc(var(--radius) + 4px)",
