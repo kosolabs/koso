d402bbb7244f67120519a89c24cafb541142293d
Shad Sharma <shadanan@gmail.com>
2025-02-14 22:46:40 -0800

    Delete shadcn-svelte Tooltip component

--- frontend/src/lib/components/ui/tooltip/tooltip-arrow.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-  import { cn } from "$lib/utils";
-  import { Tooltip as TooltipPrimitive } from "bits-ui";
-
-  const { class: classes, ...props }: TooltipPrimitive.ArrowProps = $props();
-</script>
-
-<TooltipPrimitive.Arrow class={cn("text-primary", classes)} {...props} />
