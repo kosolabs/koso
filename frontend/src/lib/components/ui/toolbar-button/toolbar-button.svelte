01e2dbb44dc5a3a65dae0c6eeefec03616cd75dd
Shad Sharma <shadanan@gmail.com>
2025-02-14 16:44:28 -0800

    Add tooltip prop to Button component (#757)

--- frontend/src/lib/components/ui/toolbar-button/toolbar-button.svelte
+++ frontend/src/lib/components/ui/toolbar-button/toolbar-button.svelte
@@ -1,29 +1,20 @@
 <script lang="ts">
   import { Button } from "$lib/kosui/button";
-  import PlainTooltip from "$lib/kosui/tooltip/plain-tooltip.svelte";
   import type { Action } from "$lib/shortcuts";
 
   const { icon, title, description, shortcut, callback }: Action = $props();
 </script>
 
-<PlainTooltip arrow>
-  {#snippet trigger(ref, props)}
-    <Button
-      bind:ref={ref.value}
-      variant="ghost"
-      {icon}
-      onclick={callback}
-      {...props}
-    >
-      <div class="max-sm:hidden">{title}</div>
-    </Button>
+<Button variant="ghost" {icon} onclick={callback}>
+  <div class="max-sm:hidden">{title}</div>
+  {#snippet tooltip()}
+    <div class="flex items-center gap-2">
+      {description}
+      {#if shortcut}
+        <div class="font-bold">
+          {shortcut.toString()}
+        </div>
+      {/if}
+    </div>
   {/snippet}
-  <div class="flex items-center gap-2">
-    <div class="text-primary-foreground">{description}</div>
-    {#if shortcut}
-      <div class="text-primary-foreground font-bold">
-        {shortcut.toString()}
-      </div>
-    {/if}
-  </div>
-</PlainTooltip>
+</Button>
