8f17d3f739b14b208edff52643ee5d2d22e3ae6f
Shad Sharma <shadanan@gmail.com>
2025-03-04 17:09:59 -0800

    Add basic tests for Kosui buttons (#843)

---
+++ frontend/src/routes/storybook/links/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import { Link, type LinkColor, type LinkUnderline } from "$lib/kosui/link";
+  import { toTitleCase } from "$lib/kosui/utils";
+
+  const linkUnderlines: LinkUnderline[] = ["always", "hover", "none"];
+  const linkColors: LinkColor[] = [
+    "primary",
+    "secondary",
+    "tertiary",
+    "error",
+    "inherit",
+  ];
+</script>
+
+<div class="flex flex-col gap-4 rounded-lg border p-4">
+  {#each linkUnderlines as underline (underline)}
+    {@const underlineTitle = toTitleCase(underline)}
+    <div>
+      <div class="mb-2">Underline {underlineTitle}</div>
+      <ul>
+        {#each linkColors as color (color)}
+          <li>
+            Here is a <Link {underline} {color} href="">{color}</Link>
+            link.
+          </li>
+        {/each}
+      </ul>
+    </div>
+  {/each}
+</div>
---
+++ frontend/tests/kosui-button-test.ts
@@ -0,0 +1,59 @@
+import { expect, test } from "@playwright/test";
+
+test.describe.configure({ mode: "parallel" });
+
+test.describe("Kosui Button Component Tests", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/storybook/buttons");
+  });
+
+  test("buttons should have correct roles", async ({ page }) => {
+    await expect(
+      page.getByRole("button", { name: "Elevated Primary Button" }),
+    ).toBeVisible();
+    await expect(
+      page.getByRole("button", { name: "Elevated Primary Icon" }),
+    ).toBeVisible();
+    await expect(
+      page.getByRole("button", { name: "Elevated Primary Disabled" }),
+    ).toBeVisible();
+  });
+
+  test("buttons should activate when enter is pressed", async ({ page }) => {
+    await page.getByTestId("result").isHidden();
+    await page.getByRole("button", { name: "Elevated Primary Button" }).focus();
+    await page.keyboard.press("Enter");
+    await expect(page.getByTestId("result")).toBeVisible();
+  });
+
+  test("buttons should activate when space is pressed", async ({ page }) => {
+    await page.getByTestId("result").isHidden();
+    await page.getByRole("button", { name: "Elevated Primary Button" }).focus();
+    await page.keyboard.press("Space");
+    await expect(page.getByTestId("result")).toBeVisible();
+  });
+
+  test("buttons should activate when clicked", async ({ page }) => {
+    await page.getByTestId("result").isHidden();
+    await page.getByRole("button", { name: "Elevated Primary Button" }).click();
+    await expect(page.getByTestId("result")).toBeVisible();
+  });
+
+  test("disabled buttons should be disabled", async ({ page }) => {
+    await expect(
+      page.getByRole("button", { name: "Elevated Primary Disabled" }),
+    ).toBeDisabled();
+  });
+
+  test("disabled buttons should not be focusable", async ({ page }) => {
+    await page
+      .getByRole("button", { name: "Elevated Primary Disabled" })
+      .focus();
+    const focusedElement = await page.evaluate(
+      () => document.activeElement?.tagName,
+    );
+    expect(focusedElement).not.toBe("BUTTON");
+  });
+
+  // TODO: Add a test for focus rings
+});
