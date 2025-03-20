02c265320d95a1991869561a3519eb5ba3306c8d
Kyle Leonhard <leonhard.kyle@gmail.com>
2025-03-20 10:48:58 -0700

    Simplify init_db

--- backend/scripts/init_db.sh
+++ backend/scripts/init_db.sh
@@ -1,22 +1,21 @@
 #!/usr/bin/env bash
 #set -x
 #set -eo pipefail
-DB_USER=${POSTGRES_USER:=postgres}
-DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
-DB_NAME="${POSTGRES_DB:=postgres}"
-DB_PORT="${POSTGRES_PORT:=5432}"
+if [ -z "${DATABASE_URL}" ]; then
+    echo "DATABASE_URL variable must be set."
+    exit 1
+fi
+
+echo "Initializing database $DATABASE_URL"
 
 # Keep pinging Postgres until it's ready to accept commands
-export PGPASSWORD="${DB_PASSWORD}"
-until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
+until psql ${DATABASE_URL} -c '\q'; do
   echo >&2 "Postgres is still unavailable - sleeping"
   sleep 1
 done
 
-echo >&2 "Postgres is up and running on port ${DB_PORT}!"
-export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
-
 if [[ -z "${KOSO_IMAGE}" ]]; then
+  echo "KOSO_IMAGE not set. Running sqlx directly."
   sqlx database create
   sqlx migrate run
 else
