50ce849a9b30cdcae957889711114905682cdeb5
Shad Sharma <shadanan@gmail.com>
2025-03-22 19:49:34 -0700

    Create an OrderedHTMLElements class to help with focus management

---
+++ frontend/src/lib/kosui/ordered-html-elements.svelte.ts
@@ -0,0 +1,35 @@
+export class OrderedHTMLElements {
+  #items: HTMLElement[] = $state.raw([]);
+
+  get items(): HTMLElement[] {
+    return this.#items;
+  }
+
+  getInsertionIndex(item: HTMLElement): number {
+    if (this.#items.includes(item)) {
+      throw new Error(`Ordered items already contains ${item}`);
+    }
+    for (let i = 0; i < this.#items.length; i++) {
+      const position = item.compareDocumentPosition(this.#items[i]);
+      if ((position & Node.DOCUMENT_POSITION_FOLLOWING) !== 0) {
+        return i;
+      }
+    }
+    return this.#items.length;
+  }
+
+  add(item: HTMLElement) {
+    console.log("add", item);
+    const index = this.getInsertionIndex(item);
+    this.#items.splice(index, 0, item);
+  }
+
+  delete(item: HTMLElement) {
+    console.log("delete", item);
+    const index = this.#items.indexOf(item);
+    if (index === -1) {
+      return;
+    }
+    this.items.splice(index, 1);
+  }
+}
