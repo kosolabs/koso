3b918d6439e04ffd707a21b5edd7a357b1c195eb
Kyle Leonhard <leonhard.kyle@gmail.com>
2025-03-24 10:27:22 -0700

    Add keys to the confetti rendering loop

--- frontend/src/lib/components/ui/confetti/confetti.svelte
+++ frontend/src/lib/components/ui/confetti/confetti.svelte
@@ -1,9 +1,10 @@
 <script module lang="ts">
   class ConfettiState {
-    rects: DOMRect[] = $state([]);
+    rects: { el: DOMRect; id: number }[] = $state([]);
+    id = 1;
 
     add(rect: DOMRect) {
-      this.rects.push(rect);
+      this.rects.push({ el: rect, id: this.id++ });
       setTimeout(() => this.rects.shift(), 2000);
     }
   }
@@ -14,9 +15,9 @@
   import Confetti from "svelte-confetti";
 </script>
 
-{#each confetti.rects as rect}
-  {@const left = rect.x + rect.width / 2}
-  {@const top = rect.y + rect.height / 2}
+{#each confetti.rects as rect (rect.id)}
+  {@const left = rect.el.x + rect.el.width / 2}
+  {@const top = rect.el.y + rect.el.height / 2}
   <div class="fixed" style="left:{left}px;top:{top}px;">
     <Confetti />
   </div>
