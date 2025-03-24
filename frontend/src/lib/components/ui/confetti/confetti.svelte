e169488b0289bcee178637162f42284c2b03c72c
Kyle Leonhard <leonhard.kyle@gmail.com>
2025-03-24 10:56:19 -0700

    Use SvelteMap to track confetti

--- frontend/src/lib/components/ui/confetti/confetti.svelte
+++ frontend/src/lib/components/ui/confetti/confetti.svelte
@@ -1,11 +1,13 @@
 <script module lang="ts">
+  const DURATION_MS = 2000;
   class ConfettiState {
-    rects: { el: DOMRect; id: number }[] = $state([]);
+    rects = $state(new SvelteMap<number, DOMRect>());
     id = 1;
 
     add(rect: DOMRect) {
-      this.rects.push({ el: rect, id: this.id++ });
-      setTimeout(() => this.rects.shift(), 2000);
+      const id = this.id++;
+      this.rects.set(id, rect);
+      setTimeout(() => this.rects.delete(id), DURATION_MS + 25);
     }
   }
   export const confetti = new ConfettiState();
@@ -13,12 +15,13 @@
 
 <script lang="ts">
   import Confetti from "svelte-confetti";
+  import { SvelteMap } from "svelte/reactivity";
 </script>
 
-{#each confetti.rects as rect (rect.id)}
-  {@const left = rect.el.x + rect.el.width / 2}
-  {@const top = rect.el.y + rect.el.height / 2}
+{#each confetti.rects as [id, rect] (id)}
+  {@const left = rect.x + rect.width / 2}
+  {@const top = rect.y + rect.height / 2}
   <div class="fixed" style="left:{left}px;top:{top}px;">
-    <Confetti />
+    <Confetti duration={DURATION_MS} x={[-0.65, 0.65]} y={[0.1, 1]} />
   </div>
 {/each}
