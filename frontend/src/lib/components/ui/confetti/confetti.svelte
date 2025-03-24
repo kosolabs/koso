1a08efd131cd9915e6abcc788de9232dfb655836
Kyle Leonhard <leonhard.kyle@gmail.com>
2025-03-24 10:59:15 -0700

    Make sure confetti doesn't abruptly disappear (#912)

--- frontend/src/lib/components/ui/confetti/confetti.svelte
+++ frontend/src/lib/components/ui/confetti/confetti.svelte
@@ -1,10 +1,13 @@
 <script module lang="ts">
+  const DURATION_MS = 2000;
   class ConfettiState {
-    rects: DOMRect[] = $state([]);
+    rects = $state(new SvelteMap<number, DOMRect>());
+    id = 1;
 
     add(rect: DOMRect) {
-      this.rects.push(rect);
-      setTimeout(() => this.rects.shift(), 2000);
+      const id = this.id++;
+      this.rects.set(id, rect);
+      setTimeout(() => this.rects.delete(id), DURATION_MS + 25);
     }
   }
   export const confetti = new ConfettiState();
@@ -12,12 +15,13 @@
 
 <script lang="ts">
   import Confetti from "svelte-confetti";
+  import { SvelteMap } from "svelte/reactivity";
 </script>
 
-{#each confetti.rects as rect}
+{#each confetti.rects as [id, rect] (id)}
   {@const left = rect.x + rect.width / 2}
   {@const top = rect.y + rect.height / 2}
   <div class="fixed" style="left:{left}px;top:{top}px;">
-    <Confetti />
+    <Confetti duration={DURATION_MS} x={[-0.65, 0.65]} y={[0.1, 1]} />
   </div>
 {/each}
