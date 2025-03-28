579731a67cbebb565d8c9844a78c596db888be71
Shad Sharma <shadanan@gmail.com>
2025-03-28 14:03:22 -0700

    Combine command-context and autocomplete-context into item-context (#931)

---
+++ frontend/src/lib/kosui/common/item-context.svelte.ts
@@ -0,0 +1,95 @@
+import { getContext, setContext } from "svelte";
+import { Bindable } from "../bindable.svelte";
+import { OrderedHTMLElements } from "../ordered-html-elements";
+import { Shortcut } from "../shortcut";
+
+export class ItemContext {
+  #input = new Bindable<string>("");
+  #items: OrderedHTMLElements = new OrderedHTMLElements();
+  #focused: HTMLElement | undefined = $state();
+
+  get input(): string {
+    return this.#input.value;
+  }
+
+  set input(value: string) {
+    this.#input.value = value;
+  }
+
+  bindInput(getInput: () => string, setInput: (val: string) => void) {
+    this.#input.bind(getInput, setInput);
+  }
+
+  get items(): HTMLElement[] {
+    return this.#items.items;
+  }
+
+  add(item: HTMLElement) {
+    this.#items.add(item);
+    this.focused = this.#input.value === "" ? undefined : this.items[0];
+    return () => this.delete(item);
+  }
+
+  delete(item: HTMLElement) {
+    this.#items.delete(item);
+    this.focused = this.#input.value === "" ? undefined : this.items[0];
+  }
+
+  get focused(): HTMLElement | undefined {
+    return this.#focused;
+  }
+
+  set focused(value: HTMLElement | undefined) {
+    this.#focused = value;
+  }
+
+  handleKeyDown(event: KeyboardEvent) {
+    if (!this.items) return;
+    if (Shortcut.ARROW_UP.matches(event)) {
+      if (this.items.length !== 0) {
+        if (!this.focused) {
+          this.focused = this.items[this.items.length - 1];
+          this.focused.scrollIntoView(false);
+        } else {
+          const index = this.items.indexOf(this.focused);
+          this.focused =
+            this.items[(index - 1 + this.items.length) % this.items.length];
+          this.focused.scrollIntoView(false);
+        }
+      }
+      event.preventDefault();
+      event.stopImmediatePropagation();
+    } else if (Shortcut.ARROW_DOWN.matches(event)) {
+      if (this.items.length !== 0) {
+        if (!this.focused) {
+          this.focused = this.items[0];
+          this.focused.scrollIntoView(false);
+        } else {
+          const index = this.items.indexOf(this.focused);
+          this.focused = this.items[(index + 1) % this.items.length];
+          this.focused.scrollIntoView(false);
+        }
+      }
+      event.preventDefault();
+      event.stopImmediatePropagation();
+    } else if (Shortcut.ENTER.matches(event)) {
+      if (this.focused) {
+        this.focused.click();
+        event.preventDefault();
+        event.stopImmediatePropagation();
+      }
+    }
+  }
+}
+
+export function newItemContext() {
+  return setItemContext(new ItemContext());
+}
+
+export function setItemContext(state: ItemContext): ItemContext {
+  return setContext<ItemContext>(ItemContext, state);
+}
+
+export function getItemContext(): ItemContext {
+  return getContext<ItemContext>(ItemContext);
+}
