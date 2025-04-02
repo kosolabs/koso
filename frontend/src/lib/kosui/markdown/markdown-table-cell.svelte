b17126b4decccd032beba1a46ee4f3b0c8f52d11
Shad Sharma <shadanan@gmail.com>
2025-04-02 10:56:18 -0700

    Add renderer for formatting table cells

---
+++ frontend/src/lib/kosui/markdown/markdown-table-cell.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import type { MarkedToken, Tokens } from "marked";
+  import type { HTMLTdAttributes, HTMLThAttributes } from "svelte/elements";
+  import type { MarkdownComponentProps } from ".";
+  import MarkdownTokens from "./markdown-tokens.svelte";
+
+  let {
+    token,
+    ...restProps
+  }: MarkdownComponentProps<Tokens.TableCell> &
+    (HTMLThAttributes | HTMLTdAttributes) = $props();
+</script>
+
+<svelte:element this={token.header ? "th" : "td"} {...restProps}>
+  <MarkdownTokens tokens={token.tokens as MarkedToken[]} />
+</svelte:element>
