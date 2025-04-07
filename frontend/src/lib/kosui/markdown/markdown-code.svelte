4f52f690172573007b804cc8874b47ac601d734b
Shad Sharma <shadanan@gmail.com>
2025-04-07 14:21:31 -0700

    koso-729: Add syntax highlighting for code blocks in markdown view (#969)

--- frontend/src/lib/kosui/markdown/markdown-code.svelte
+++ frontend/src/lib/kosui/markdown/markdown-code.svelte
@@ -1,13 +1,20 @@
 <script lang="ts">
+  import hljs from "highlight.js";
   import type { Tokens } from "marked";
   import type { HTMLAttributes } from "svelte/elements";
   import type { MarkdownComponentProps } from ".";
+  import "./theme.css";
 
   let {
     token,
     ...restProps
   }: MarkdownComponentProps<Tokens.Code> & HTMLAttributes<HTMLPreElement> =
     $props();
+
+  const code = $derived(
+    hljs.highlight(token.text, { language: token.lang || "plaintext" }).value,
+  );
 </script>
 
-<pre {...restProps}><code class={`lang-${token.lang}`}>{token.text}</code></pre>
+<!-- eslint-disable-next-line svelte/no-at-html-tags -->
+<pre {...restProps}><code class={`lang-${token.lang}`}>{@html code}</code></pre>
