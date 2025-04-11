27e98aa4e8610974e4469a708cc4593256e26acd
Kyle Leonhard <leonhard.kyle@gmail.com>
2025-04-11 09:58:50 -0700

    Split planning context tests from koso tests (#991)

--- frontend/src/lib/dag-table/koso.svelte.test.ts
+++ frontend/src/lib/dag-table/koso.svelte.test.ts
@@ -1,14 +1,12 @@
 import type { User } from "$lib/auth.svelte";
 import type { Kind } from "$lib/yproxy";
-import { List, Set } from "immutable";
-import * as encoding from "lib0/encoding";
+import { Set } from "immutable";
 import { uuidv4 } from "lib0/random.js";
 import { beforeEach, describe, expect, it } from "vitest";
 import * as Y from "yjs";
 import { Koso, Node } from ".";
-import { type TaskBuilder } from "../../../tests/utils";
+import { EMPTY_SYNC_RESPONSE, type TaskBuilder } from "../../../tests/utils";
 import { TaskLinkage } from "./koso.svelte";
-import { PlanningContext } from "./planning-context.svelte";
 
 const USER: User = {
   email: "t@koso.app",
@@ -24,18 +22,8 @@ const OTHER_USER: User = {
   exp: 0,
 };
 
-const EMPTY_SYNC_RESPONSE = (() => {
-  const encoder = encoding.createEncoder();
-  encoding.writeVarUint(encoder, 0);
-  encoding.writeVarUint(encoder, 1);
-  encoding.writeVarUint8Array(encoder, Y.encodeStateAsUpdateV2(new Y.Doc()));
-  return encoding.toUint8Array(encoder);
-})();
-
 describe("Koso tests", () => {
-  let root: Node;
   let koso: Koso;
-  let planningCtx: PlanningContext;
 
   const init = (tasks: TaskBuilder[]) => {
     const upsertedTaskIds = Set<string>(tasks.map((t) => t.id));
@@ -78,8 +66,6 @@ describe("Koso tests", () => {
   beforeEach((context) => {
     const cleanup = $effect.root(() => {
       koso = new Koso("project-id-" + uuidv4(), new Y.Doc());
-      planningCtx = new PlanningContext(koso);
-      root = planningCtx.root;
       koso.setSendAndSync(() => {});
       koso.receive(EMPTY_SYNC_RESPONSE);
     });
@@ -374,7 +360,7 @@ describe("Koso tests", () => {
 
   describe("insertTask", () => {
     it("creates a child of root", () => {
-      const id1 = koso.insertTask(root.name, 0, USER, "Task 1");
+      const id1 = koso.insertTask("root", 0, USER, "Task 1");
       expect(koso.toJSON()).toEqual({
         root: {
           id: "root",
@@ -451,14 +437,14 @@ describe("Koso tests", () => {
     });
   });
 
-  describe("linkNode", () => {
+  describe("link", () => {
     it("link node 2 to node 1 succeeds", () => {
       init([
         { id: "root", name: "Root", children: ["1", "2"] },
         { id: "1", name: "Task 1" },
         { id: "2", name: "Task 2" },
       ]);
-      planningCtx.linkNode(Node.parse("2"), Node.parse("1"), 0);
+      koso.link(new TaskLinkage({ parentId: "1", id: "2" }), 0);
 
       expect(koso.toJSON()).toMatchObject({
         root: { children: ["1", "2"] },
@@ -473,7 +459,7 @@ describe("Koso tests", () => {
         { id: "1", name: "Task 1" },
       ]);
       expect(() =>
-        planningCtx.linkNode(Node.parse("1"), Node.parse("1"), 0),
+        koso.link(new TaskLinkage({ parentId: "1", id: "1" }), 0),
       ).toThrow();
     });
 
@@ -484,7 +470,7 @@ describe("Koso tests", () => {
         { id: "2", name: "Task 2" },
       ]);
       expect(() =>
-        planningCtx.linkNode(Node.parse("1"), Node.parse("2"), 0),
+        koso.link(new TaskLinkage({ parentId: "2", id: "1" }), 0),
       ).toThrow();
     });
 
@@ -507,12 +493,11 @@ describe("Koso tests", () => {
         { id: "2", name: "Some PR", kind: "github_pr" },
       ]);
       expect(() =>
-        planningCtx.linkNode(Node.parse("1"), Node.parse("github"), 0),
+        koso.link(new TaskLinkage({ parentId: "github", id: "1" }), 0),
       ).toThrow();
       expect(() =>
-        planningCtx.linkNode(
-          Node.parse("1"),
-          Node.parse("github/github_pr"),
+        koso.link(
+          new TaskLinkage({ parentId: "github/github_pr", id: "1" }),
           0,
         ),
       ).toThrow();
@@ -537,9 +522,8 @@ describe("Koso tests", () => {
         { id: "2", name: "Some PR", kind: "github_pr" },
       ]);
       expect(() =>
-        planningCtx.linkNode(
-          Node.parse("1"),
-          Node.parse("github/github_pr/2"),
+        koso.link(
+          new TaskLinkage({ parentId: "github/github_pr/2", id: "1" }),
           0,
         ),
       ).toThrow();
@@ -563,13 +547,9 @@ describe("Koso tests", () => {
         },
         { id: "2", name: "Some PR", kind: "github_pr" },
       ]);
-      planningCtx.linkNode(
-        Node.parse("github/github_pr/2"),
-        Node.parse("1"),
-        0,
-      );
-      planningCtx.linkNode(Node.parse("github/github_pr"), Node.parse("1"), 0);
-      planningCtx.linkNode(Node.parse("github"), Node.parse("1"), 0);
+      koso.link(new TaskLinkage({ parentId: "1", id: "2" }), 0);
+      koso.link(new TaskLinkage({ parentId: "1", id: "github_pr" }), 0);
+      koso.link(new TaskLinkage({ parentId: "1", id: "github" }), 0);
       expect(koso.toJSON()).toMatchObject({
         root: { children: ["1", "github"] },
         ["github"]: { children: ["github_pr"] },
@@ -579,7 +559,7 @@ describe("Koso tests", () => {
     });
   });
 
-  describe("moveNode", () => {
+  describe("move", () => {
     it("move node 3 to child of node 1 as a peer of node 2 succeeds (reparent)", () => {
       init([
         { id: "root", name: "Root", children: ["1", "3"] },
@@ -588,7 +568,7 @@ describe("Koso tests", () => {
         { id: "3", name: "Task 3" },
       ]);
 
-      planningCtx.moveNode(Node.parse("3"), Node.parse("1"), 1);
+      koso.move("3", "root", "1", 1);
 
       expect(koso.toJSON()).toMatchObject({
         root: { children: ["1"] },
@@ -606,7 +586,7 @@ describe("Koso tests", () => {
         { id: "3", name: "Task 3" },
       ]);
 
-      planningCtx.moveNode(Node.parse("3"), Node.parse("1"), 0);
+      koso.move("3", "root", "1", 0);
 
       expect(koso.toJSON()).toMatchObject({
         root: { children: ["1"] },
@@ -625,7 +605,7 @@ describe("Koso tests", () => {
         { id: "4", name: "Task 4" },
       ]);
 
-      planningCtx.moveNode(Node.parse("1/4"), Node.parse("1/3"), 0);
+      koso.move("4", "1", "3", 0);
 
       expect(koso.toJSON()).toMatchObject({
         root: { children: ["1"] },
@@ -643,9 +623,7 @@ describe("Koso tests", () => {
         { id: "2", name: "Task 2" },
       ]);
 
-      expect(() =>
-        planningCtx.moveNode(Node.parse("2"), Node.parse("1"), 1),
-      ).toThrow();
+      expect(() => koso.move("2", "root", "1", 1)).toThrow();
     });
 
     it("move node 4 to be the peer of node 2 succeeds (reorder)", () => {
@@ -657,7 +635,7 @@ describe("Koso tests", () => {
         { id: "4", name: "Task 4" },
       ]);
 
-      planningCtx.moveNode(Node.parse("1/4"), Node.parse("1"), 1);
+      koso.move("4", "1", "1", 1);
 
       expect(koso.toJSON()).toMatchObject({
         root: { children: ["1"] },
@@ -677,7 +655,7 @@ describe("Koso tests", () => {
         { id: "4", name: "Task 4" },
       ]);
 
-      planningCtx.moveNode(Node.parse("1/3"), Node.parse("1"), 3);
+      koso.move("3", "1", "1", 3);
 
       expect(koso.toJSON()).toMatchObject({
         root: { children: ["1"] },
@@ -706,23 +684,9 @@ describe("Koso tests", () => {
         },
         { id: "2", name: "Some PR", kind: "github_pr" },
       ]);
-      expect(() =>
-        planningCtx.moveNode(Node.parse("github"), Node.parse("1"), 0),
-      ).toThrow();
-      expect(() =>
-        planningCtx.moveNode(
-          Node.parse("github/github_pr"),
-          Node.parse("1"),
-          0,
-        ),
-      ).toThrow();
-      expect(() =>
-        planningCtx.moveNode(
-          Node.parse("github/github_pr/2"),
-          Node.parse("1"),
-          0,
-        ),
-      ).toThrow();
+      expect(() => koso.move("github", "root", "1", 0)).toThrow();
+      expect(() => koso.move("github_pr", "github", "1", 0)).toThrow();
+      expect(() => koso.move("2", "github_pr", "1", 0)).toThrow();
     });
 
     it("move non-canonical plugin task/container succeeds", () => {
@@ -748,9 +712,9 @@ describe("Koso tests", () => {
         },
         { id: "2", name: "Some PR", kind: "github_pr" },
       ]);
-      planningCtx.moveNode(Node.parse("2"), Node.parse("1"), 0);
-      planningCtx.moveNode(Node.parse("github_pr"), Node.parse("1"), 0);
-      planningCtx.moveNode(Node.parse("3/github"), Node.parse("1"), 0);
+      koso.move("2", "root", "1", 0);
+      koso.move("github_pr", "root", "1", 0);
+      koso.move("github", "3", "1", 0);
       expect(koso.toJSON()).toMatchObject({
         root: { children: ["1", "github"] },
         ["github"]: { children: ["github_pr"] },
@@ -770,7 +734,7 @@ describe("Koso tests", () => {
           kind: "github",
         },
       ]);
-      planningCtx.moveNode(Node.parse("github"), Node.parse("root"), 0);
+      koso.move("github", "root", "root", 0);
       expect(koso.toJSON()).toMatchObject({
         root: { children: ["github", "1"] },
       });
@@ -795,11 +759,7 @@ describe("Koso tests", () => {
         { id: "2", name: "Some PR", kind: "github_pr" },
         { id: "3", name: "Some PR", kind: "github_pr" },
       ]);
-      planningCtx.moveNode(
-        Node.parse("github/github_pr/3"),
-        Node.parse("github/github_pr"),
-        0,
-      );
+      koso.move("3", "github_pr", "github_pr", 0);
       expect(koso.toJSON()).toMatchObject({
         ["github_pr"]: { children: ["3", "2"] },
       });
@@ -1498,77 +1458,6 @@ describe("Koso tests", () => {
     });
   });
 
-  describe("toNodes", () => {
-    beforeEach(() => {
-      planningCtx.expanded = Set([]);
-    });
-
-    it("empty doc has no nodes", () => {
-      expect(planningCtx.nodes).toStrictEqual(List([root]));
-    });
-
-    it("doc with one task has one node", () => {
-      init([{ id: "root", name: "Root", children: ["1"] }]);
-      expect(planningCtx.nodes).toStrictEqual(List([root, Node.parse("1")]));
-    });
-
-    it("doc with non-visible tasks returns root", () => {
-      init([
-        { id: "root", name: "Root", children: ["1"] },
-        { id: "1", name: "Task 1", status: "Done" },
-      ]);
-      expect(planningCtx.nodes).toStrictEqual(List([root]));
-    });
-
-    it("doc with two tasks has two nodes", () => {
-      init([
-        { id: "root", name: "Root", children: ["1", "2"] },
-        { id: "1", name: "Task 1" },
-        { id: "2", name: "Task 2" },
-      ]);
-      expect(planningCtx.nodes).toStrictEqual(
-        List([root, Node.parse("1"), Node.parse("2")]),
-      );
-    });
-
-    it("doc with two tasks and one subtask has two nodes", () => {
-      init([
-        { id: "root", name: "Root", children: ["1", "2"] },
-        { id: "1", name: "Task 1", children: ["3"] },
-        { id: "2", name: "Task 2" },
-        { id: "3", name: "Task 3" },
-      ]);
-      expect(planningCtx.nodes).toStrictEqual(
-        List([root, Node.parse("1"), Node.parse("2")]),
-      );
-    });
-
-    it("doc with two tasks, one subtask, and parent is expanded has three nodes", () => {
-      init([
-        { id: "root", name: "Root", children: ["1", "2"] },
-        { id: "1", name: "Task 1", children: ["3"] },
-        { id: "2", name: "Task 2" },
-        { id: "3", name: "Task 3" },
-      ]);
-      planningCtx.expanded = Set([Node.parse("1")]);
-      expect(planningCtx.nodes).toStrictEqual(
-        List([root, Node.parse("1"), Node.parse("1/3"), Node.parse("2")]),
-      );
-    });
-
-    it("doc with two tasks, one linked subtask, and parent is expanded has three nodes", () => {
-      init([
-        { id: "root", name: "Root", children: ["1", "2"] },
-        { id: "1", name: "Task 1", children: ["2"] },
-        { id: "2", name: "Task 2" },
-      ]);
-      planningCtx.expanded = Set([Node.parse("1")]);
-      expect(planningCtx.nodes).toStrictEqual(
-        List([root, Node.parse("1"), Node.parse("1/2"), Node.parse("2")]),
-      );
-    });
-  });
-
   describe("toJSON", () => {
     it("empty graph renders successfully", () => {
       expect(koso.toJSON()).toMatchObject({
@@ -1935,147 +1824,6 @@ describe("Koso tests", () => {
           assignee: null,
         },
       });
-    });
-  });
-
-  describe("getPrevLink", { sequential: true }, () => {
-    it("loops through expanded nodes", () => {
-      init([
-        {
-          id: "root",
-          name: "Root",
-          children: ["t1", "t2", "t3", "t4", "t5", "t6"],
-        },
-        { id: "t2" },
-        { id: "t4", children: ["t2"] },
-        { id: "t6", children: ["t2"] },
-      ]);
-      planningCtx.expanded = Set([Node.parse("t4"), Node.parse("t6")]);
-
-      let node: Node | null = Node.parse("t2");
-      node = planningCtx.getPrevLink(node);
-      expect(node).toEqual(Node.parse("t6/t2"));
-      if (!node) throw new Error();
-
-      node = planningCtx.getPrevLink(node);
-      expect(node).toEqual(Node.parse("t4/t2"));
-      if (!node) throw new Error();
-
-      node = planningCtx.getPrevLink(node);
-      expect(node).toEqual(Node.parse("t2"));
-    });
-
-    it("ignores collapsed nodes", () => {
-      init([
-        {
-          id: "root",
-          name: "Root",
-          children: ["t1", "t2", "t3", "t4", "t5", "t6"],
-        },
-        { id: "t2" },
-        { id: "t4", children: ["t2"] },
-        { id: "t6", children: ["t2"] },
-      ]);
-      planningCtx.expanded = Set([Node.parse("t4")]);
-
-      let node: Node | null = Node.parse("t2");
-      node = planningCtx.getPrevLink(node);
-      expect(node).toEqual(Node.parse("t4/t2"));
-      if (!node) throw new Error();
-
-      node = planningCtx.getPrevLink(node);
-      expect(node).toEqual(Node.parse("t2"));
-    });
-
-    it("does not repeat single node", () => {
-      init([
-        {
-          id: "root",
-          name: "Root",
-          children: ["t1", "t2", "t3"],
-        },
-        { id: "t2" },
-      ]);
-
-      expect(planningCtx.getPrevLink(Node.parse("t2"))).toBeNull();
-    });
-  });
-
-  describe("getNextLink", { sequential: true }, () => {
-    it("loops through expanded nodes", () => {
-      init([
-        {
-          id: "root",
-          name: "Root",
-          children: ["t1", "t2", "t3", "t4", "t5", "t6"],
-        },
-        { id: "t2" },
-        { id: "t4", children: ["t2"] },
-        { id: "t6", children: ["t2"] },
-      ]);
-      planningCtx.expanded = Set([Node.parse("t4"), Node.parse("t6")]);
-
-      let node: Node | null = Node.parse("t2");
-      node = planningCtx.getNextLink(node);
-      expect(node).toEqual(Node.parse("t4/t2"));
-      if (!node) throw new Error();
-
-      node = planningCtx.getNextLink(node);
-      expect(node).toEqual(Node.parse("t6/t2"));
-      if (!node) throw new Error();
-
-      node = planningCtx.getNextLink(node);
-      expect(node).toEqual(Node.parse("t2"));
-    });
-
-    it("ignores collapsed nodes", () => {
-      init([
-        {
-          id: "root",
-          name: "Root",
-          children: ["t1", "t2", "t3", "t4", "t5", "t6"],
-        },
-        { id: "t2" },
-        { id: "t4", children: ["t2"] },
-        { id: "t6", children: ["t2"] },
-      ]);
-      planningCtx.expanded = Set([Node.parse("t4")]);
-
-      let node: Node | null = Node.parse("t2");
-      node = planningCtx.getNextLink(node);
-      expect(node).toEqual(Node.parse("t4/t2"));
-      if (!node) throw new Error();
-
-      node = planningCtx.getNextLink(node);
-      expect(node).toEqual(Node.parse("t2"));
-    });
-
-    it("does not repeat single node", () => {
-      init([
-        {
-          id: "root",
-          name: "Root",
-          children: ["t1", "t2", "t3"],
-        },
-        { id: "t2" },
-      ]);
-
-      expect(planningCtx.getNextLink(Node.parse("t2"))).toBeNull();
-    });
-  });
-
-  describe("getNextLink", { sequential: true }, () => {
-    it("does not repeat single node", () => {
-      init([
-        {
-          id: "root",
-          name: "Root",
-          children: ["t1", "t2", "t3"],
-        },
-        { id: "t2" },
-      ]);
-
-      expect(planningCtx.getNextLink(Node.parse("t2"))).toBeNull();
     });
   });
 
