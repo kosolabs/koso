966179fee81103100c24fe1b6586f718582ebb28
Shad Sharma <shadanan@gmail.com>
2025-05-19 18:54:57 -0700

    Double click on Markdown button toggles the viewer (#1102)

--- frontend/src/lib/dag-table/desc-action.svelte
+++ frontend/src/lib/dag-table/desc-action.svelte
@@ -11,12 +11,19 @@
   };
   let { task }: Props = $props();
 
+  let open = $state(false);
+
   const prefs = getPrefsContext();
 </script>
 
 {#if task.desc}
-  <Tooltip rich click class="max-h-2/5 max-w-3/5 overflow-y-scroll p-1">
-    {#snippet trigger(props)}
+  <Tooltip
+    bind:open
+    rich
+    click
+    class="max-h-2/5 max-w-3/5 overflow-y-scroll p-1"
+  >
+    {#snippet trigger({ onclick, ...restProps })}
       <Button
         class="m-0 p-2"
         variant="plain"
@@ -24,8 +31,19 @@
         shape="circle"
         aria-label="Show task description panel"
         icon={task.desc !== null ? FileText : FilePlus2}
-        onclick={() => (prefs.detailPanel = "view")}
-        {...props}
+        onclick={(event) => {
+          onclick?.();
+          event?.stopImmediatePropagation();
+        }}
+        ondblclick={() => {
+          if (prefs.detailPanel === "none") {
+            prefs.detailPanel = "view";
+          } else {
+            prefs.detailPanel = "none";
+          }
+          open = false;
+        }}
+        {...restProps}
       />
     {/snippet}
     <MarkdownViewer class="p-1" value={task.desc.toString()} />
