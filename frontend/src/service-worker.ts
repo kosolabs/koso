218c6cda4d10fca8eb97fdd96e935e9ec985e5d7
Kyle Leonhard <leonhard.kyle@gmail.com>
2025-05-20 15:23:47 -0700

    koso-902: Add more SW logs

--- frontend/src/service-worker.ts
+++ frontend/src/service-worker.ts
@@ -32,18 +32,23 @@ const precache_list = ["/", ...build, ...files, ...prerendered].map((s) => ({
   revision: version,
 }));
 
+console.debug("precacheAndRoute");
 precacheAndRoute(precache_list);
 
+console.debug("cleanupOutdatedCaches");
 cleanupOutdatedCaches();
 
 // Cache user avatar images
+console.debug("registerRoute googleusercontent");
 registerRoute(
   ({ url }) => url.hostname.endsWith("googleusercontent.com"),
   new StaleWhileRevalidate(),
 );
 
 // Serve the app from the precache
+console.debug("registerRoute /");
 registerRoute(new NavigationRoute(createHandlerBoundToURL("/")));
 
 // Serve requests to /api from the network first, and from the cache if offline.
+console.debug("registerRoute /api");
 registerRoute(({ url }) => url.pathname.startsWith("/api"), new NetworkFirst());
