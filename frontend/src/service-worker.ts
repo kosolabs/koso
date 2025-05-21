f012d5fd08bcff1094a2ecedbe8de1076f54430a
Kyle Leonhard <leonhard.kyle@gmail.com>
2025-05-21 10:50:04 -0700

    koso-902: Fix update hangs by untracking toasts (#1112)

--- frontend/src/service-worker.ts
+++ frontend/src/service-worker.ts
@@ -16,7 +16,7 @@ import { NetworkFirst, StaleWhileRevalidate } from "workbox-strategies";
 const sw = self as unknown as ServiceWorkerGlobalScope;
 sw.__WB_DISABLE_DEV_LOGS = true;
 
-console.debug("Running service worker script");
+console.debug("Setting up service worker");
 
 sw.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
@@ -32,23 +32,18 @@ const precache_list = ["/", ...build, ...files, ...prerendered].map((s) => ({
   revision: version,
 }));
 
-console.debug("precacheAndRoute");
 precacheAndRoute(precache_list);
 
-console.debug("cleanupOutdatedCaches");
 cleanupOutdatedCaches();
 
 // Cache user avatar images
-console.debug("registerRoute googleusercontent");
 registerRoute(
   ({ url }) => url.hostname.endsWith("googleusercontent.com"),
   new StaleWhileRevalidate(),
 );
 
 // Serve the app from the precache
-console.debug("registerRoute /");
 registerRoute(new NavigationRoute(createHandlerBoundToURL("/")));
 
 // Serve requests to /api from the network first, and from the cache if offline.
-console.debug("registerRoute /api");
 registerRoute(({ url }) => url.pathname.startsWith("/api"), new NetworkFirst());
