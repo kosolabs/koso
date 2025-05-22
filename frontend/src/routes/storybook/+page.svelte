940a41f355606d78cb4c1b47a1afbeeaf4b60144
Shad Sharma <shadanan@gmail.com>
2025-05-22 12:26:55 -0700

    koso-932: Use hamburger menu to navigate around storybook and hide when no items

--- frontend/src/routes/storybook/+page.svelte
+++ frontend/src/routes/storybook/+page.svelte
@@ -1,28 +1,7 @@
 <script lang="ts">
   import { Goto } from "$lib/kosui/goto";
   import { toTitleCase } from "$lib/kosui/utils";
-
-  const paths: string[] = [
-    "/storybook/alerts",
-    "/storybook/autocomplete",
-    "/storybook/avatar",
-    "/storybook/badge",
-    "/storybook/buttons",
-    "/storybook/chips",
-    "/storybook/code-mirror",
-    "/storybook/command",
-    "/storybook/dialogs",
-    "/storybook/fab",
-    "/storybook/goto",
-    "/storybook/inputs",
-    "/storybook/links",
-    "/storybook/markdown",
-    "/storybook/menus",
-    "/storybook/progress-indicators",
-    "/storybook/shortcuts",
-    "/storybook/toggles",
-    "/storybook/tooltips",
-  ];
+  import { paths } from "./+layout.svelte";
 </script>
 
 <div class="flex flex-col items-start rounded-lg border p-4">
