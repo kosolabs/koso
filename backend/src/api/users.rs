2d22ab2c147675ff068865fe6b53955d263f6b13
Kyle Leonhard <leonhard.kyle@gmail.com>
2025-05-23 10:41:50 -0700

    Add an update user endpoint

--- backend/src/api/users.rs
+++ backend/src/api/users.rs
@@ -1,16 +1,21 @@
 use crate::api::{ApiResult, google, model::User, not_found_error, verify_premium};
-use axum::{Extension, Json, Router, extract::Path, routing::get};
+use axum::{
+    Extension, Json, Router,
+    extract::Path,
+    routing::{get, patch},
+};
 use sqlx::postgres::PgPool;
 
-use super::{bad_request_error, unauthorized_error};
+use super::{bad_request_error, model::UpdateUser, unauthorized_error};
 
 pub(super) fn router() -> Router {
     Router::new()
         .route("/", get(list_users_handler))
         .route("/{email}", get(get_user_handler))
+        .route("/{email}", patch(update_user_handler))
 }
 
-#[tracing::instrument(skip(pool))]
+#[tracing::instrument(skip(pool, user))]
 async fn list_users_handler(
     Extension(pool): Extension<&'static PgPool>,
     Extension(user): Extension<google::User>,
@@ -26,21 +31,13 @@ async fn list_users_handler(
     Ok(Json(users))
 }
 
-#[tracing::instrument(skip(pool))]
+#[tracing::instrument(skip(pool, user))]
 async fn get_user_handler(
     Extension(pool): Extension<&'static PgPool>,
     Extension(user): Extension<google::User>,
     Path(email): Path<String>,
 ) -> ApiResult<Json<User>> {
-    if email.is_empty() {
-        return Err(bad_request_error("EMPTY_EMAIL", "Email must not be empty"));
-    }
-    if email != user.email {
-        return Err(unauthorized_error(&format!(
-            "User {} is not authorized to access {}",
-            user.email, email
-        )));
-    }
+    authorize_user(&user, &email)?;
 
     let user: Option<User> =
         sqlx::query_as("SELECT email, name, picture, premium FROM users WHERE email=$1;")
@@ -54,4 +51,42 @@ async fn get_user_handler(
             &format!("User {email} not found"),
         )),
     }
+}
+
+#[tracing::instrument(skip(pool, user))]
+async fn update_user_handler(
+    Extension(pool): Extension<&'static PgPool>,
+    Extension(user): Extension<google::User>,
+    Path(email): Path<String>,
+    Json(update_user): Json<UpdateUser>,
+) -> ApiResult<()> {
+    authorize_user(&user, &email)?;
+
+    let res = sqlx::query(
+        "
+        UPDATE users
+        SET github_login = $2
+        WHERE email = $1",
+    )
+    .bind(&user.email)
+    .bind(&update_user.github_login)
+    .execute(pool)
+    .await?;
+    if res.rows_affected() == 0 {
+        return Err(not_found_error("NOT_FOUND", "User does not exist."));
+    }
+    Ok(())
+}
+
+fn authorize_user(user: &google::User, email: &str) -> ApiResult<()> {
+    if email.is_empty() {
+        return Err(bad_request_error("EMPTY_EMAIL", "Email must not be empty"));
+    }
+    if email != user.email {
+        return Err(unauthorized_error(&format!(
+            "User {} is not authorized to access {}",
+            user.email, email
+        )));
+    }
+    Ok(())
 }
