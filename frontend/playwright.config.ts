6542e2494a4e915db9c7967001eaf20714f382f2
Kyle Leonhard <4945355+kyle-leonhard@users.noreply.github.com>
2025-07-06 10:46:35 -0700

    Configure host setting for playwright

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -1,17 +1,20 @@
 import type { PlaywrightTestConfig } from "@playwright/test";
 
+const port = process.env.CI ? 3000 : process.env.PW_SERVER_PORT || 5173;
+
 const config: PlaywrightTestConfig = {
   webServer: {
     command: process.env.KOSO_IMAGE
       ? `docker run \
         --env KOSO_ENV=dev \
         --env KOSO_SETTING_DATABASE_URL \
+        --env KOSO_SETTING_HOST=localhost:${port} \
         --env RUST_LOG=info \
         -v ${process.env.KOSO_SETTING_SECRETS_DIR}:/.secrets \
         --network=host \
         --rm ${process.env.KOSO_IMAGE}`
       : "pnpm run build && (cd ../backend && ./scripts/run_dev.sh)",
-    port: process.env.CI ? 3000 : process.env.PW_SERVER_PORT || 5173,
+    port,
     reuseExistingServer: !process.env.CI,
     stdout: "pipe",
     timeout: 180000,
