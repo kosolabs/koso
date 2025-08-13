a4233153f47f6d3465dbabb5f59e9baf2d54e7ba
Anvita Deenadayalan <187209505+hianvita@users.noreply.github.com>
2025-08-13 13:33:37 -0700

    Embedding Working!

--- dedupe/dedupe.py
+++ dedupe/dedupe.py
@@ -1,5 +1,4 @@
 async def embeddings_notebook() :
-# %%
     import asyncio
     import pandas as pd
     import json
@@ -13,37 +12,24 @@ async def embeddings_notebook() :
 
     client = openai.AsyncOpenAI()
 
-    # %%
     response = await client.embeddings.create(model="text-embedding-3-small", input="The food was delicious and the waiter...", encoding_format="float")
 
-    # %%
     response
 
-    # %%
     with open("koso-dogfood-export-2025-7-5-12-2.json") as new: 
         embedding = json.load(new)
 
-    # %%
     tasks = embedding['graph']
 
-
-
-    # %%
-    tasks
-
-    # %%
     for _, task in tasks.items():
         print(task["name"])
 
-    # %%
     async def get_embedding(data): 
         response = await client.embeddings.create(model="text-embedding-3-small", input=data, encoding_format="float")
         return response.data[0].embedding
 
-    # %%
     await get_embedding("Koso will be revolutionary!")
 
-    # %%
     async def process_row(row, db, semaphore=asyncio.Semaphore(1)):
         async with semaphore:
             embedding = await get_embedding(row["name"])
@@ -59,19 +45,13 @@ async def embeddings_notebook() :
             df.to_sql('embeddings',db, if_exists='append')
             return df
 
-
-    # %%
     def get_processed_row_ids(db):
         try: 
             return {row[0] for row in db.execute('Select id from embeddings').fetchall()}
         except sqlite3.OperationalError as e: 
-            print(f"Skipping delta due to error getting previously processed Id's: {3}")
+            print(f"Skipping delta due to error getting previously processed Id's: {e}")
         return set()
 
-    # %% [markdown]
-    # 
-
-    # %%
     semaphore = asyncio.Semaphore(20)
 
     db_file = 'koso-tasks-embeddings.sqlite'
@@ -86,15 +66,11 @@ async def embeddings_notebook() :
         coros.append(process_row(task, db, semaphore))
     await tqdm_asyncio.gather(*coros)
 
-
-    # %%
     tasks.items()
 
-    # %%
     for _, task in tasks.items(): 
         print(task['id'])
 
-    # %%
 
 
 
