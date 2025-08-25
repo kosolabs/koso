494d090ad62220a7d3239833c61450edcce11e5c
Kyle Leonhard <4945355+kyle-leonhard@users.noreply.github.com>
2025-08-25 11:45:59 -0700

    Exit bash script immediately on command failure (#1313)

--- backend/scripts/init_db.sh
+++ backend/scripts/init_db.sh
@@ -1,18 +1,12 @@
 #!/usr/bin/env bash
-#set -x
-#set -eo pipefail
+set -eo pipefail
+
 if [ -z "${DATABASE_URL}" ]; then
     echo "DATABASE_URL variable must be set."
     exit 1
 fi
 
 echo "Initializing database $DATABASE_URL"
-
-# Keep pinging Postgres until it's ready to accept commands
-until psql ${DATABASE_URL} -c '\q'; do
-  echo >&2 "Postgres is still unavailable - sleeping"
-  sleep 1
-done
 
 if [[ -z "${KOSO_IMAGE}" ]]; then
   echo "KOSO_IMAGE not set. Running sqlx directly."
