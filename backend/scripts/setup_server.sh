c617c482bc34d167c810e3ca3e2c17d9a2e0fb69
Kyle Leonhard <4945355+kyle-leonhard@users.noreply.github.com>
2025-08-25 13:59:42 -0700

    Adjust notes for trixie (#1316)

--- backend/scripts/setup_server.sh
+++ backend/scripts/setup_server.sh
@@ -43,8 +43,8 @@ apt update &&\
 apt install ca-certificates curl gnupg apt-transport-https gpg
 curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker.gpg
 apt update
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker.gpg] https://download.docker.com/linux/debian bookworm stable" |tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker.gpg] https://download.docker.com/linux/debian trixie stable" |tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin
 systemctl status docker
 systemctl is-active docker && echo "Docker running"
 
