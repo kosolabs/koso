203bb9e8655eb256b9f1f67ffe007a7da32e483b
Kyle Leonhard <4945355+kyle-leonhard@users.noreply.github.com>
2025-08-25 11:32:46 -0700

    Remove unecessary health check

--- backend/scripts/init_db.sh
+++ backend/scripts/init_db.sh
@@ -8,12 +8,6 @@ fi
 
 echo "Initializing database $DATABASE_URL"
 
-# Keep pinging Postgres until it's ready to accept commands
-until psql ${DATABASE_URL} -c '\q'; do
-  echo >&2 "Postgres is still unavailable - sleeping"
-  sleep 1
-done
-
 if [[ -z "${KOSO_IMAGE}" ]]; then
   echo "KOSO_IMAGE not set. Running sqlx directly."
   sqlx database create
